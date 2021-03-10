05b58efeb0772014c5ac727214fbabb135aba4b9
Swathi Chittlur <schittlur@deloitte.com>
2021-03-10 15:18:43 -0500

    header resources moved

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,8 +2,8 @@ import React from 'react';
 import classes from './Header.module.css';
 import { NavLink } from 'react-router-dom';
 import { Link } from 'react-router-dom'
-import logo from './dodLogo.png';
-import searchButton from './search.svg';
+import logo from '../../resources/internal/dodLogo.png';
+import searchButton from '../../resources/internal/search.svg';
 
 
 const Header = (props) => {
