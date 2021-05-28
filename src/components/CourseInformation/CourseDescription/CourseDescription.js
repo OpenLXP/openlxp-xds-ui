35ddc1a84a75b2b1c837401a45e89338a240f94b
rglasgow-deloitte <rglasgow@deloitte.com>
2021-05-28 08:52:34 -0400

    Adds test script for CourseDescription.js

--- src/components/CourseInformation/CourseDescription/CourseDescription.js
+++ src/components/CourseInformation/CourseDescription/CourseDescription.js
@@ -1,19 +1,19 @@
 import classes from "../CourseInformation.module.css";
 
 const CourseDescription = (props) => {
-	// Expects a course desc to be passed in
-	const desc = props.desc;
+  // Expects a course desc to be passed in
+  const desc = props.desc;
 
-	if (!desc) {
-		return null;
-	}
+  if (!desc) {
+    return null;
+  }
 
-	return (
-		<div className={classes.desc} data-testid="CourseDescription">
-			<h4>Course Description</h4>
-			<p >{desc}</p>
-		</div>
-	);
+  return (
+    <div className={classes.desc}>
+      <h4>Course Description</h4>
+      <p>{desc}</p>
+    </div>
+  );
 };
 
 export default CourseDescription;
