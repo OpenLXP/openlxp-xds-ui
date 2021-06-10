c2a5a24dc2f349408d942e84cfc1946d3cdea8c6
Swathi Chittlur <schittlur@deloitte.com>
2021-06-10 16:52:00 -0400

    dropdown

--- src/components/SearchResultsPage/SearchResultsPage.js
+++ src/components/SearchResultsPage/SearchResultsPage.js
@@ -335,28 +335,28 @@ const SearchResultPage = (props) => {
         }
     }
 
-    //sort functionality dropdown 
+    //sort functionality dropdown
     let filterDropdownn = null;
-    if (coursesState.coursesObj != null && coursesState.coursesObj.total > 0){
+    if (coursesState.coursesObj != null && coursesState.coursesObj.total > 0) {
         filterDropdownn = (
-            <Select options={options} value={filterDropdown}
-            placeholder="Select an option" className='dropdown'
-            onChange={event => {
-                setFilterDropdown(event);
-                let paramObj  = {};
-                //if sort is not in the url, then it is added 
-                if (paramObj['sort'] === null){
-                    const updatedParamObj = getUpdatedSearchQuery(location, paramObj, true);
-                    const searchString = getSearchString(updatedParamObj);
-                    history.push({
-                        pathname: '/search/',
-                        search: searchString + "&sort=" + event.value
-                    });
-                }
-                //if sort is already in the url, replace value 
-                else{
-                    if (event.value === "MostRelevant"){
-                        paramObj['sort'] = null;
+            <Select 
+                    options={options} value={filterDropdown}
+                    placeholder="Select an option" className='dropdown'
+                    onChange={(event) => {
+                    setFilterDropdown(event);
+                    let paramObj = {};
+                    //if sort is not in the url, then it is added
+                    if (paramObj["sort"] === null) {
+                        const updatedParamObj = getUpdatedSearchQuery(
+                            location,
+                            paramObj,
+                            true
+                        );
+                        const searchString = getSearchString(updatedParamObj);
+                        history.push({
+                            pathname: "/search/",
+                            search: searchString + "&sort=" + event.value,
+                        });
                     }
                     //if sort is already in the url, replace value
                     else {
@@ -445,7 +445,7 @@ const SearchResultPage = (props) => {
                             </Link>
                         </div>
                     </div>
-                    {filterDropdown}
+                    {filterDropdownn}
                     {expPanelContent}
                     {pagination}
                 </div>
