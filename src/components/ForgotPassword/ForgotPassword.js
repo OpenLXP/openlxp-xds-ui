529f6d9b3ffdba1820b69e53b47ec598344c0767
Swathi Chittlur <schittlur@deloitte.com>
2021-06-11 15:49:56 -0400

    Forgot password page

---
+++ src/components/ForgotPassword/ForgotPassword.js
@@ -0,0 +1,37 @@
+import classes from "./ForgotPassword.module.css";
+import { useState } from "react";
+
+const ForgotPassword = (props) => {
+    let [credentials, setCredentials] = useState({
+        username: "",
+        password: "",
+      });
+    
+      const handleResetPassword = () => {
+        alert("Reset Password!");
+        props.history.push("/dashboard");
+      };
+      const handleLogin = () => {
+        props.history.push("/signIn");
+      };
+
+  return (
+    <div className={classes.container}>
+        <div className={classes.title}>Forgot Password</div>
+        <form className={classes.inputWrapper}>
+          <input className={classes.input} placeholder={"Username/Email"} />
+        </form>
+        <div onClick={handleResetPassword} className={classes.loginBtn}>
+          Reset Password
+        </div>
+        <div className={classes.text}>or
+            <a onClick={handleLogin} className={classes.link}>
+            Login
+            </a>
+      </div>
+        
+    </div>
+  );
+};
+
+export default ForgotPassword;
