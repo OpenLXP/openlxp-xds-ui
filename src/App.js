e0fb28359b15e7b96b93d65c212e9fe2515957e9
Swathi Chittlur <schittlur@deloitte.com>
2021-06-11 15:49:36 -0400

    Sign Up Page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import Layout from "./hoc/Layout/Layout";
 import LandingPage from "./components/LandingPage/LandingPage";
 import SearchResultPage from "./components/SearchResultsPage/SearchResultsPage";
 import CourseInformation from "./components/CourseInformation/CourseInformation";
+import SignIn from "./components/SignIn/SignIn";
+import ForgotPassword from "./components/ForgotPassword/ForgotPassword";
+import SignUp from "./components/SignUp/SignUp";
 
 function App() {
   const dispatch = useDispatch();
@@ -19,9 +22,11 @@ function App() {
       <Route path="/about" />
       <Route path="/resources" />
       <Route path="/help" />
-      <Route path="/signIn" />
+      <Route path="/signIn" component={SignIn}/>
       <Route path="/course" component={CourseInformation} />
       <Route path="/" exact component={LandingPage} />
+      <Route path="/forgotPassword" component={ForgotPassword} />
+      <Route path="/Signup" component={SignUp} />
       <Redirect to="/" />
     </Switch>
   );
