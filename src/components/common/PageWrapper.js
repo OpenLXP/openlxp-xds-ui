3120ce4f285595e18867caa4fa738fccbf65b6ac
rglasgow-deloitte <rglasgow@deloitte.com>
2021-06-24 10:31:51 -0400

    Adds page wrapper component

--- src/components/common/PageWrapper.js
+++ src/components/common/PageWrapper.js
@@ -1,5 +1,11 @@
 const PageWrapper = (props) => {
-    return <div className="px-32">{props.children}</div>;
+    return (
+        <div className="px-4 md:px-24 lg:px-32">
+            <div className="bg-white px-2">{props.children}</div>
+        </div>
+    );
 };
 
 export default PageWrapper;
+
+
