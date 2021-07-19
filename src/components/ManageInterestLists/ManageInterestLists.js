c70ab1eb0c5ed5d46f9934158ec6ed46d47eda88
rglasgow-deloitte <rglasgow@deloitte.com>
2021-07-19 09:21:26 -0400

    Adds loading component to the

--- src/components/ManageInterestLists/ManageInterestLists.js
+++ src/components/ManageInterestLists/ManageInterestLists.js
@@ -15,10 +15,63 @@ const ManageInterestLists = (props) => {
   const { user, lists } = useSelector((state) => state);
 
   // states for individual lists
-  const [currentLists, setCurrentLists] = useState({
-    user: [],
-    subscribed: [],
-  });
+  const [currentLists, setCurrentLists] = useState(null);
+
+  let mainContent = (
+    <>
+      <div className="bg-white rounded-md px-4 pb-8 my-4">
+        <h2 className="font-semibold font-sans text-xl py-3">My Lists</h2>
+        <div className="space-y-6">
+          {currentLists?.user.map((list) => {
+            return <InterestLists list={list} />;
+          })}
+        </div>
+      </div>
+      <div className="bg-white rounded-md px-4 pb-8 my-4">
+        <h2 className="font-semibold font-sans text-xl py-3">
+          Subscibed Lists
+        </h2>
+        <div className="space-y-6">
+          {currentLists?.subscribed.map((list) => {
+            return <SubscribedLists list={list} />;
+          })}
+        </div>
+      </div>
+    </>
+  );
+
+  let loadingContent = (
+    <>
+      <div className="w-full bg-white rounded-md px-4 pb-4 py-3 my-4 animate-pulse space-y-2">
+        <div className="rounded-md h-8 w-36 bg-opacity-30 bg-light-blue" />
+        <div className="border border-light-blue border-opacity-50 rounded-md pl-1 pr-4 py-1">
+          <div className="flex flex-row justify-between items-center text-light-blue my-2 text-opacity-50">
+            <div className="rounded-md h-8 w-56 bg-opacity-30 bg-light-blue" />
+            <ion-icon name="chevron-down-outline" />
+          </div>
+          <div className="flex flex-row justify-between items-center">
+            <div className="w-36 h-8 rounded-md my-2 bg-opacity-30 bg-light-blue"></div>
+            <div className="w-16 h-8 rounded-md my-2 bg-opacity-30 bg-light-blue"></div>
+          </div>
+          <div className="w-full h-36 rounded-md my-2 bg-light-blue bg-opacity-30"></div>
+        </div>
+      </div>
+      <div className="w-full bg-white rounded-md px-4 pb-4 py-3 my-4 animate-pulse space-y-2">
+        <div className="rounded-md h-8 w-36 bg-opacity-30 bg-light-blue" />
+        <div className="border border-light-blue border-opacity-50 rounded-md pl-1 pr-4 py-1">
+          <div className="flex flex-row justify-between items-center text-light-blue my-2 text-opacity-50">
+            <div className="rounded-md h-8 w-56 bg-opacity-30 bg-light-blue" />
+            <ion-icon name="chevron-down-outline" />
+          </div>
+          <div className="flex flex-row justify-between items-center">
+            <div className="w-36 h-8 rounded-md my-2 bg-opacity-30 bg-light-blue"></div>
+            <div className="w-16 h-8 rounded-md my-2 bg-opacity-30 bg-light-blue"></div>
+          </div>
+          <div className="w-full h-36 rounded-md my-2 bg-light-blue bg-opacity-30"></div>
+        </div>
+      </div>
+    </>
+  );
 
   useEffect(() => {
     // if the user is not logged in redirect to login
@@ -28,17 +81,20 @@ const ManageInterestLists = (props) => {
 
     // if the user profile is present
     if (user?.user && user.status === "succeeded") {
+      console.log(lists);
       if (!lists?.lists) {
         dispatch(getLists());
       }
 
-      if (lists.status === "succeeded") {
-        setCurrentLists(lists?.lists);
-      }
+      setTimeout(() => {
+        if (lists.status === "succeeded") {
+          setCurrentLists(lists?.lists);
+        }
+      }, 2000);
     }
 
     // Re-render if user state changes or if list state changes
-  }, [lists?.lists, user?.user]);
+  }, [lists?.status, user?.user]);
 
   // Generating Interest Lists from the user data saved on backend
   // TODO: Move this to its own component
@@ -50,24 +106,10 @@ const ManageInterestLists = (props) => {
           <h1 className="font-semibold font-sans text-3xl my-4">
             Manage Interest Lists
           </h1>
-          <div className="bg-white rounded-md px-4 pb-8">
-            <h2 className="font-semibold font-sans text-xl py-3">My Lists</h2>
-            <div className="space-y-6">
-              {currentLists.user.map((list) => {
-                return <InterestLists list={list} />;
-              })}
-            </div>
-          </div>
-          <div className="bg-white rounded-md px-4 pb-8 my-4">
-            <h2 className="font-semibold font-sans text-xl py-3">
-              Subscibed Lists
-            </h2>
-            <div className="space-y-6">
-              {currentLists.subscribed.map((list) => {
-                return <SubscribedLists list={list} />;
-              })}
-            </div>
-          </div>
+
+          {currentLists?.user || currentLists?.subscribed
+            ? mainContent
+            : loadingContent}
         </>
       ) : null}
     </PageWrapper>
