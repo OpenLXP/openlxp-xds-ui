fb4da998c5092f5b921e980f40024ab2aa71a776
rglasgow-deloitte <rglasgow@deloitte.com>
2021-08-04 11:32:47 -0400

    Removes stale code from user list.js

--- src/store/user.js
+++ src/store/user.js
@@ -99,58 +99,6 @@ export const userSlice = createSlice({
         firstName: action.payload.user.first_name,
         lastName: action.payload.user.last_name,
         token: action.payload.token,
-        userLists: [
-          {
-            title: "Maths",
-            id: 1,
-            owner: "John Doe",
-            updated: "1/12/2022",
-            description:
-              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis iste assumenda nulla voluptas provident voluptatibus ratione deserunt tempora pariatur perferendis ullam praesentium asperiores hic officia, qui illum commodi laboriosam, labore?",
-            courses: [
-              {
-                title: "Calculus 1A",
-                provider: "Han Solo",
-                id: "1",
-              },
-              {
-                title: "Calculus 1B",
-                provider: "C3P0",
-                id: "2",
-              },
-              {
-                title: "Calculus 1C",
-                provider: "Chewbacca",
-                id: "3",
-              },
-              {
-                title: "Calculus 1E",
-                provider: "Yoda",
-                id: "4",
-              },
-            ],
-          },
-          {
-            title: "Data List",
-            id: 2,
-            owner: "John Doe",
-            updated: "1/12/2022",
-            description:
-              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis iste assumenda nulla voluptas provident voluptatibus ratione deserunt tempora pariatur perferendis ullam praesentium asperiores hic officia, qui illum commodi laboriosam, labore? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta cupiditate aperiam expedita aliquid nisi illum eligendi deleniti at ipsam, ratione rem molestiae sit officia repellat dolore dolor voluptate consequuntur hic.",
-            courses: [
-              {
-                title: "Data and the world around you",
-                provider: "Yoda",
-                id: "5",
-              },
-              {
-                title: "Informed decision making using data analytics",
-                provider: "Yoda of dagoba",
-                id: "6",
-              },
-            ],
-          },
-        ],
       };
     },
     [loginUser.rejected]: (state, action) => {
