66c36fb51a4fee200b682221d820bda59ea8e350
schittlur <78377530+schittlur@users.noreply.github.com>
2021-08-05 10:11:02 -0400

    moved lists to top of file for better organization

--- src/components/CourseInformation/InterestGroupPopup/InterestGroupPopup.test.js
+++ src/components/CourseInformation/InterestGroupPopup/InterestGroupPopup.test.js
@@ -10,6 +10,30 @@ const useSelectorMock = jest.spyOn(redux, "useSelector");
 jest.mock("axios");
 
 let container = null;
+let lists = [
+  {
+    id: 1,
+    owner: {
+      id: 2,
+      email: "test@test.com",
+      first_name: "test",
+      last_name: "user",
+    },
+    subscribers: [],
+    created: "2021-07-23T14:30:08.814573Z",
+    modified: "2021-07-23T14:40:45.576233Z",
+    description: "New Test List Desc",
+    name: "Test List",
+    experiences: ["4765692fbbeb11a0033cf39aea8ece8f"],
+  },
+];
+let user = {
+  email: "test@test.com",
+  first_name: "test",
+  last_name: "test",
+  token: "tokeneything",
+};
+
 beforeEach(() => {
   const mockIntersectionObserver = jest.fn();
   mockIntersectionObserver.mockReturnValue({
@@ -32,30 +56,6 @@ afterEach(() => {
 
 test("does render", async () => {
   await act(async () => {
-    let lists = [
-      {
-        id: 1,
-        owner: {
-          id: 2,
-          email: "test@test.com",
-          first_name: "test",
-          last_name: "user",
-        },
-        subscribers: [],
-        created: "2021-07-23T14:30:08.814573Z",
-        modified: "2021-07-23T14:40:45.576233Z",
-        description: "New Test List Desc",
-        name: "Test List",
-        experiences: ["4765692fbbeb11a0033cf39aea8ece8f"],
-      },
-    ];
-    let user = {
-      email: "test@test.com",
-      first_name: "test",
-      last_name: "test",
-      token: "tokeneything",
-    };
-
     useSelectorMock.mockReturnValue({ lists: lists });
     axios.get.mockReturnValue({ lists: lists });
     useSelectorMock.mockReturnValue({ user: user });
@@ -67,29 +67,6 @@ test("does render", async () => {
 
 test("does open new modal on click", async () => {
   await act(async () => {
-    let lists = [
-      {
-        id: 1,
-        owner: {
-          id: 2,
-          email: "test@test.com",
-          first_name: "test",
-          last_name: "user",
-        },
-        subscribers: [],
-        created: "2021-07-23T14:30:08.814573Z",
-        modified: "2021-07-23T14:40:45.576233Z",
-        description: "New Test List Desc",
-        name: "Test List",
-        experiences: ["4765692fbbeb11a0033cf39aea8ece8f"],
-      },
-    ];
-    let user = {
-      email: "test@test.com",
-      first_name: "test",
-      last_name: "test",
-      token: "tokeneything",
-    };
     let state = { lists: lists, user: user };
 
     useSelectorMock.mockReturnValue(state);
@@ -133,29 +110,6 @@ test("does open new modal on click", async () => {
 
 test("does add new list", async () => {
   await act(async () => {
-    let lists = [
-      {
-        id: 1,
-        owner: {
-          id: 2,
-          email: "test@test.com",
-          first_name: "test",
-          last_name: "user",
-        },
-        subscribers: [],
-        created: "2021-07-23T14:30:08.814573Z",
-        modified: "2021-07-23T14:40:45.576233Z",
-        description: "New Test List Desc",
-        name: "Test List",
-        experiences: ["4765692fbbeb11a0033cf39aea8ece8f"],
-      },
-    ];
-    let user = {
-      email: "test@test.com",
-      first_name: "test",
-      last_name: "test",
-      token: "tokeneything",
-    };
     let state = { lists: lists, user: user };
 
     useSelectorMock.mockReturnValue(state);
@@ -174,44 +128,6 @@ test("does add new list", async () => {
     fireEvent.change(screen.getByPlaceholderText("New List Description"), {
       target: { value: "New Title" },
     });
-    let lists = [
-      {
-        id: 1,
-        owner: {
-          id: 2,
-          email: "test@test.com",
-          first_name: "test",
-          last_name: "user",
-        },
-        subscribers: [],
-        created: "2021-07-23T14:30:08.814573Z",
-        modified: "2021-07-23T14:40:45.576233Z",
-        description: "New Test List Desc",
-        name: "Test List",
-        experiences: ["4765692fbbeb11a0033cf39aea8ece8f"],
-      },
-      {
-        id: 2,
-        owner: {
-          id: 2,
-          email: "test@test.com",
-          first_name: "test",
-          last_name: "user",
-        },
-        subscribers: [],
-        created: "2021-07-23T14:30:08.814573Z",
-        modified: "2021-07-23T14:40:45.576233Z",
-        description: "New Test List Desc",
-        name: "Test List",
-        experiences: [],
-      },
-    ];
-    let user = {
-      email: "test@test.com",
-      first_name: "test",
-      last_name: "test",
-      token: "tokeneything",
-    };
     let state = { lists: lists, user: user };
     
     useSelectorMock.mockReturnValue(state);
