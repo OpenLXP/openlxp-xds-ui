d98921e70fed6971dc96dc8617128030cde03bbe
rglasgow-deloitte <rglasgow@deloitte.com>
2021-08-05 17:01:05 -0400

    Refactors the course details page and updates tests.

--- src/components/CourseInformation/CourseButton/CourseButton.js
+++ src/components/CourseInformation/CourseButton/CourseButton.js
@@ -1,10 +1,14 @@
 const CourseButton = (props) => {
-	const url = props.url;
-
-	return (
-		<a href={url} target="blank_" rel="noreferrer" className="btn">
-			View Course
-		</a>
-	);
+  const url = props.url;
+  console.log(url);
+  return (
+    <a
+      href={url}
+      target="blank_"
+      rel="noreferrer"
+      className="w-full block bg-base-blue text-center text-white py-2 rounded-md">
+      View Course
+    </a>
+  );
 };
 export default CourseButton;
