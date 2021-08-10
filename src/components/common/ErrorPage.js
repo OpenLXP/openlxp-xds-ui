5aa09a83cd27a82277573bf37d98d8d65315ffb1
rglasgow-deloitte <rglasgow@deloitte.com>
2021-08-10 10:58:43 -0400

    Increases the padding to the error component to be more centered.

--- src/components/common/ErrorPage.js
+++ src/components/common/ErrorPage.js
@@ -3,7 +3,7 @@ import PageWrapper from "./PageWrapper";
 export default function ErrorPage({ children }) {
   return (
     <PageWrapper className="bg-body-gray">
-      <div className="flex flex-col justify-center my-56 items-center">
+      <div className="flex flex-col justify-center my-72 items-center">
         <div className="text-4xl">
           <ion-icon name="alert-circle-outline" />
         </div>
