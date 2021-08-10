70f9c2a686922cfce76191f0683b2d9817121d09
rglasgow-deloitte <rglasgow@deloitte.com>
2021-08-10 10:57:39 -0400

    Updates image component to render the same size element each time.

--- src/components/CourseInformation/CourseImage/CourseImage.js
+++ src/components/CourseInformation/CourseImage/CourseImage.js
@@ -1,49 +1,14 @@
-import classes from "../CourseInformation.module.css";
-import { useSelector } from "react-redux";
-import ExpPreviewPanel from "./../../SearchResultsPage/ExpPreviewPanel/ExpPreviewPanel";
-
-
-const CourseImage = (props) => {
-  // const img = props.img;
-  // if (!img) {
-  //   return (
-  //     <div
-  //       data-testid="image"
-  //       className={
-  //         "w-64 h-32 bg-gradient-to-br from-base-blue to-dark-blue rounded-md"
-  //       }
-  //     />
-  //   );
-  // }
-  // return <img className="rounded-sm" src={img} alt={"Course"} />;
-
-  const exp = props.coursesInfo.data;
-  const { configuration } = useSelector((state) => state.configuration);
-  const backendHost = process.env.REACT_APP_BACKEND_HOST;
-
-  if (exp.Technical_Information && exp.Technical_Information.Thumbnail) {
-  return (
-    <ExpPreviewPanel
-      expObj={exp}
-      imgLink={exp.Technical_Information.Thumbnail}
-    />
-  );
-  } else {
-    if (configuration && configuration.course_img_fallback) {
-      return (
-        <ExpPreviewPanel
-          expObj={exp}
-          imgLink={backendHost + configuration.course_img_fallback}
-        />
-      );
-    } else {
-      return (
-        <ExpPreviewPanel 
-          expObj={exp} 
-          />
-      );
-    }
+const CourseImage = ({ image }) => {
+  if (!image) {
+    return (
+      <div
+        data-testid="image"
+        className="w-64 h-32 mt-1.5 bg-gradient-to-br from-base-blue to-dark-blue rounded-md"
+      />
+    );
   }
+  return <img className="rounded-sm w-64 h-32 mt-1.5" src={image}
+              alt={"Course"}/>;
 };
 
 export default CourseImage;
