6b6c2ffc99dcca47cedb9d559dbbb0bcaeb33c1b
rglasgow-deloitte <rglasgow@deloitte.com>
2021-08-16 16:43:19 -0400

    Adds searchable user lists to subscribe to

--- src/components/SearchInterestLists/SearchInterestLists.js
+++ src/components/SearchInterestLists/SearchInterestLists.js
@@ -3,38 +3,19 @@ import { useState } from "react";
 import { useSelector } from "react-redux";
 import PageWrapper from "../common/PageWrapper";
 import axios from "axios";
-
-const testData = [
-  { title: "title 1", owner: "list owner" },
-  { title: "title 2", owner: "list owner 2" },
-  { title: "title 3", owner: "list owner 3" },
-  { title: "title 4", owner: "list owner 4" },
-  { title: "title 5", owner: "list owner 5" },
-  { title: "title 6", owner: "list owner 6" },
-  { title: "title 7", owner: "list owner 7" },
-  { title: "title 8", owner: "list owner 8" },
-  { title: "title 9", owner: "list owner 9" },
-  { title: "title 10", owner: "list owner 10" },
-  { title: "title 11", owner: "list owner 11" },
-  { title: "title 12", owner: "list owner 12" },
-  { title: "title 13", owner: "list owner 13" },
-  { title: "title 14", owner: "list owner 14" },
-  { title: "title 15", owner: "list owner 15" },
-  { title: "title 16", owner: "list owner 16" },
-  { title: "title 17", owner: "list owner 17" },
-  { title: "title 18", owner: "list owner 18" },
-  { title: "title 19", owner: "list owner 19" },
-  { title: "title 20", owner: "list owner 20" },
-  { title: "title 21", owner: "list owner 21" },
-  { title: "title 22", owner: "list owner 22" },
-];
+import { useHistory } from "react-router-dom";
+import InterestList from "./InterestList/InterestList";
 
 export default function SearchInterestLists() {
+  const history = useHistory();
+
   const [origionalInterestLists, setOriginalInterestLists] = useState([]);
   const [interestLists, setInterestLists] = useState([]);
+  const [subscribedLists, setSubscribedLists] = useState([]);
   const [page, setPage] = useState(0);
   const [search, setSearch] = useState("");
   const { configuration } = useSelector((state) => state.configuration);
+  const { user } = useSelector((state) => state.user);
 
   // chunks array into sub chunks
   const chunkArray = (arr) => {
@@ -56,45 +37,59 @@ export default function SearchInterestLists() {
 
   const filterArray = (origionalArr) => {
     const array = [...origionalArr];
+    // console.log(array)
 
-    return array.filter(
-      ({ title, owner }) => title.includes(search) || owner.includes(search)
-    );
+    if (array) {
+      return array.filter(
+        ({ name, owner }) =>
+          name?.includes(search) ||
+          owner.first_name.includes(search) ||
+          owner.last_name.includes(search)
+      );
+    }
   };
 
-  // On initial load
-  // set the origional data
-  // On cleanup
-  // set the interest list
+  // On load...
+  useEffect(async () => {
+    if (user) {
+      // get all the lists from the backend
+      axios
+        .get(process.env.REACT_APP_INTEREST_LISTS_ALL)
+        .then((resp) => {
+          // saving the data
+          setOriginalInterestLists(resp.data);
+          // chunking the array
+          const chunked = chunkArray(resp.data);
+          setInterestLists(chunked);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+
+      const url = `${process.env.REACT_APP_USER_SUBSCRIPTION_LISTS}`;
+      let header = {
+        Authorization: "Token " + user.token,
+      };
+      axios
+        .get(url, { headers: header })
+        .then((resp) => {
+          // checking if a user is subscribed to this list
+          setSubscribedLists(resp.data);
+          console.log(subscribedLists);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      history.push("/");
+    }
+  }, []);
+
+  // On update of search
   useEffect(() => {
-    // check if user is logged in
-
-    // make api call to the
-
-    // set the origional list set
-
-    setOriginalInterestLists(testData);
-    return () => {
-      setInterestLists(chunkArray(testData));
-    };
-  }, [origionalInterestLists]);
-
-  useEffect( () => {
-    // filter through the array
-    const filteredArray = filterArray(origionalInterestLists);
-    // chunk the array into parts
-    const chunked = chunkArray(filteredArray);
-    // set the new interest list
-    // setInterestLists(chunked);
-    axios
-      .get(
-        `${process.env.REACT_APP_INTEREST_LISTS_ALL}`,
-      )
-      .then((response) => {
-        console.log(response.data);
-        setInterestLists(response.data.experiences);
-      });
-
+    const filteredLists = filterArray(origionalInterestLists);
+    const chunckedLists = chunkArray(filteredLists);
+    setInterestLists(chunckedLists);
   }, [search]);
 
   return (
@@ -108,7 +103,8 @@ export default function SearchInterestLists() {
           setSearch(event.target.value);
         }}
       />
-      <div className="flex flex-row justify-end gap-5 my-2 pb-2">
+
+      <div className="flex flex-row justify-end gap-4 my-2 pb-2">
         <div
           className={`${
             page < 1 && "invisible"
@@ -121,7 +117,7 @@ export default function SearchInterestLists() {
         <div className="select-none">{page + 1}</div>
         <div
           className={`${
-            page === interestLists.length - 1 && "invisible"
+            page === interestLists?.length - 1 && "invisible"
           } bg-base-blue text-white px-2 rounded-md select-none cursor-pointer`}
           onClick={() => {
             setPage(page + 1);
@@ -129,19 +125,19 @@ export default function SearchInterestLists() {
           next
         </div>
       </div>
-      <div className="flex flex-col divide-y">
+
+      <div className="space-y-6 pb-10">
         {interestLists[page]?.map((list) => {
-          return (
-            <div className="flex flex-row justify-between items-center">
-              <div>
-                <div className="tracking-widest">{list.title}</div>
-                <div className="tracking-widest">{list.owner}</div>
-              </div>
-              <div className="flex flex-row items-center bg-base-blue bg-opacity-10 text-base-blue px-2 rounded-md h-6 hover:bg-base-blue hover:bg-opacity-100 hover:text-white cursor-pointer transition-colors duration-300">
-                Subscribe
-              </div>
-            </div>
-          );
+          if (!subscribedLists.find((slist) => slist.id === list.id)) {
+            return (
+              <InterestList
+                title={list.name}
+                owner={list.owner}
+                desc={list.desc}
+                id={list.id}
+              />
+            );
+          }
         })}
       </div>
     </PageWrapper>
