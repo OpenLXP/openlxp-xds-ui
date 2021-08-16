63445d08a136a4994ade129ec5ef2dd6e22d2613
rglasgow-deloitte <rglasgow@deloitte.com>
2021-08-16 17:46:38 -0400

    Updates env vars used to call apis

--- src/components/CourseInformation/CourseInformation.js
+++ src/components/CourseInformation/CourseInformation.js
@@ -1,8 +1,8 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import {useLocation} from "react-router-dom";
-import {useSelector} from "react-redux";
-import {useParams} from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 
 import CourseImage from "./CourseImage/CourseImage";
 import CourseButton from "./CourseButton/CourseButton";
@@ -13,7 +13,6 @@ import PageWrapper from "../common/PageWrapper";
 import ErrorPage from "../common/ErrorPage";
 
 const CourseInformation = () => {
-
   // Get the global config
   const { configuration } = useSelector((state) => state.configuration);
   const { user } = useSelector((state) => state.user);
@@ -57,7 +56,7 @@ const CourseInformation = () => {
           data: resp.data,
           isLoading: false,
           error: null,
-        })
+        });
       })
       .catch((err) => {
         setCourseInfo({
@@ -68,7 +67,6 @@ const CourseInformation = () => {
       });
 
     if (!courseInfo.error) {
-
       // Making call to back end for related courses
       axios
         .get(process.env.REACT_APP_ES_MLT_API + id)
@@ -77,7 +75,7 @@ const CourseInformation = () => {
             data: resp.data,
             isLoading: false,
             error: null,
-          })
+          });
         })
         .catch((err) => {
           setRelatedCourses({
@@ -112,20 +110,28 @@ const CourseInformation = () => {
   if (configuration) {
     const courseDataMappings = configuration.course_information;
     courseDetails = {
-      title: getCourseDataMapping(courseDataMappings?.course_title, courseInfo.data),
-      url: getCourseDataMapping(courseDataMappings?.course_url, courseInfo.data),
+      title: getCourseDataMapping(
+        courseDataMappings?.course_title,
+        courseInfo.data
+      ),
+      url: getCourseDataMapping(
+        courseDataMappings?.course_url,
+        courseInfo.data
+      ),
       desc: getCourseDataMapping(
         courseDataMappings.course_description,
         courseInfo.data
       ),
     };
-    courseDetails.supplmentary = configuration?.course_highlights.map((item) => {
-      return {
-        icon: getIconNameToUse(item.highlight_icon),
-        name: item.display_name,
-        value: getCourseDataMapping(item.field_name, courseInfo.data) || "",
-      };
-    });
+    courseDetails.supplmentary = configuration?.course_highlights.map(
+      (item) => {
+        return {
+          icon: getIconNameToUse(item.highlight_icon),
+          name: item.display_name,
+          value: getCourseDataMapping(item.field_name, courseInfo.data) || "",
+        };
+      }
+    );
   }
 
   // return an error if the courseInfo has an error
@@ -134,46 +140,46 @@ const CourseInformation = () => {
       <ErrorPage>
         Error loading course data. Please contact an administrator.
       </ErrorPage>
-    )
+    );
   }
 
   let configImage = null;
-  if(configuration && configuration.course_img_fallback){
-    configImage = process.env.REACT_APP_BACKEND_HOST + configuration?.course_img_fallback;
+  if (configuration && configuration.course_img_fallback) {
+    configImage =
+      process.env.REACT_APP_BACKEND_HOST + configuration?.course_img_fallback;
   }
-  
+
   // render if everything is 'ok'
   return (
     <PageWrapper>
       <div className="px-2 py-5">
-        <h2 className="font-semibold text-2xl my-2">
-          {courseDetails.title}
-        </h2>
+        <h2 className="font-semibold text-2xl my-2">{courseDetails.title}</h2>
         <div className="">
           <div className="float-left space-y-2 pr-5 pb-1">
             <CourseImage
-              image={courseInfo.data?.Technical_Information?.Thumbnail || configImage}/>
-            <CourseButton url={courseDetails.url}/>
-            {user && <InterestGroupPopup/>}
+              image={
+                courseInfo.data?.Technical_Information?.Thumbnail || configImage
+              }
+            />
+            <CourseButton url={courseDetails.url} />
+            {console.log(courseDetails)}
+            {user && <InterestGroupPopup courseId={id} />}
           </div>
           <h3 className="text-left text-lg font-semibold mb-1">
             Course Description
           </h3>
-          <p className="text-xs">
-            {courseDetails.desc}
-          </p>
+          <p className="text-xs">{courseDetails.desc}</p>
         </div>
       </div>
-      <div className="border-b py-2 clear-both my-2"/>
+      <div className="border-b py-2 clear-both my-2" />
       <div className="px-2 clear-both">
-        <div
-          className="flex flex-row flex-wrap justify-start items-baseline gap-2">
+        <div className="flex flex-row flex-wrap justify-start items-baseline gap-2">
           {courseDetails.supplmentary?.map((detail, index) => (
-            <CourseDetails detail={detail} key={index}/>
+            <CourseDetails detail={detail} key={index} />
           ))}
         </div>
       </div>
-      {relatedCourses.data && <RelatedCourses data={relatedCourses.data}/>}
+      {relatedCourses.data && <RelatedCourses data={relatedCourses.data} />}
       {!relatedCourses?.data && <div>Loading...</div>}
     </PageWrapper>
   );
