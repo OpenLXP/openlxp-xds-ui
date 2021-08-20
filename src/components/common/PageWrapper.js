a6e8b1426112c6b2d039e9f2280419a0b9664436
rglasgow-deloitte <rglasgow@deloitte.com>
2021-08-20 14:53:11 -0400

    Updates page wrapper to use relative and removes white background

--- src/components/common/PageWrapper.js
+++ src/components/common/PageWrapper.js
@@ -1,8 +1,8 @@
 const PageWrapper = (props) => {
-    const style = `bg-white px-2 ${props.className}`;
+    const style = `px-2 relative ${props.className}`;
 
     return (
-        <div className="px-4 md:px-24 lg:px-32 xl:px-56">
+        <div className="px-4 md:px-24 lg:px-32 xl:px-56 bg-transparent">
             <div className={style}>{props.children}</div>
         </div>
     );
