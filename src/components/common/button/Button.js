3b67103ded6b2e6c37509cfc028ae6f18e17b69e
rglasgow-deloitte <rglasgow@deloitte.com>
2021-08-23 13:41:01 -0400

    Adds test for buttons.

--- src/components/common/button/Button.js
+++ src/components/common/button/Button.js
@@ -1,7 +1,17 @@
-export default function Button({ onClick, children }) {
+export default function Button({ onClick, children, fontWeight }) {
+  if (fontWeight) {
+    return (
+      <div
+        className={`py-2 px-3 text-center shadow-sm text-white bg-base-blue rounded-md cursor-pointer whitespace-nowrap text-base hover:bg-dark-blue hover:bg-opacity-90 transition-colors duration-300 ease-in-out font-${fontWeight}`}
+        onClick={onClick}>
+        {children}
+      </div>
+    );
+  }
+
   return (
     <div
-      className="py-2 px-3 text-center shadow-sm text-white bg-base-blue rounded-md cursor-pointer whitespace-nowrap text-base hover:bg-dark-blue hover:bg-opacity-90 transition-colors duration-300 ease-in-out font-semibold"
+      className={`py-2 px-3 text-center shadow-sm text-white bg-base-blue rounded-md cursor-pointer whitespace-nowrap text-base hover:bg-dark-blue hover:bg-opacity-90 transition-colors duration-300 ease-in-out font-semibold`}
       onClick={onClick}>
       {children}
     </div>
