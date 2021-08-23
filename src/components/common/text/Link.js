16f885a23e00ac9b2d8318a6211307d9693712a9
rglasgow-deloitte <rglasgow@deloitte.com>
2021-08-23 13:42:49 -0400

    Adds tests for text components

--- src/components/common/text/Link.js
+++ src/components/common/text/Link.js
@@ -1,38 +1,18 @@
 export default function Link({ children, onClick, size = null }) {
   let style = "text-base-blue hover:text-bright-blue text-sm cursor-pointer";
 
-  switch (size) {
-    case "lg":
-      return (
-        <a onClick={onClick} className={`${style} text-${size}`}>
-          {children}
-        </a>
-      );
-    case "md":
-      return (
-        <a onClick={onClick} className={`${style} text-${size}`}>
-          {children}
-        </a>
-      );
-    case "sm":
-      return (
-        <a onClick={onClick} className={`${style} text-${size}`}>
-          {children}
-        </a>
-      );
-    case "xs":
-      return (
-        <a onClick={onClick} className={`${style} text-${size}`}>
-          {children}
-        </a>
-      );
-    default:
-      return (
-        <a
-          onClick={onClick}
-          className="text-base-blue hover:text-bright-blue cursor-pointer">
-          {children}
-        </a>
-      );
+  if (size) {
+    return (
+      <a onClick={onClick} className={`${style} text-${size}`}>
+        {children}
+      </a>
+    );
   }
+  return (
+    <a
+      onClick={onClick}
+      className="text-base-blue hover:text-bright-blue cursor-pointer text-base">
+      {children}
+    </a>
+  );
 }
