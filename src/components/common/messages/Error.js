620aeb5e2a357f66dace53c525c7e953d27e1903
rglasgow-deloitte <rglasgow@deloitte.com>
2021-08-23 13:41:48 -0400

    Adds test for messages, removes unused code.

--- src/components/common/messages/Error.js
+++ src/components/common/messages/Error.js
@@ -1,5 +1,5 @@
-export default function Error({ error }) {
+export default function Error({ children }) {
   return <div className="font-thin text-xs text-red-500 mt-1 ml-1">
-    {error}
+    {children}
   </div>
 }
