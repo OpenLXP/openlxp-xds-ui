132a9860d4e9aa3c8c39d0a1c4d80c51d29cdb40
rglasgow-deloitte <rglasgow@deloitte.com>
2021-08-23 10:22:11 -0400

    Refactors PageWrapper.js to use de-structured props.

--- src/components/common/PageWrapper.js
+++ src/components/common/PageWrapper.js
@@ -1,9 +1,9 @@
-const PageWrapper = (props) => {
-    const style = `px-2 relative ${props.className}`;
+const PageWrapper = ({className, children}) => {
+    const style = `px-2 relative ${className}`;
 
     return (
         <div className="px-4 md:px-24 lg:px-32 xl:px-56 bg-transparent">
-            <div className={style}>{props.children}</div>
+            <div className={style}>{children}</div>
         </div>
     );
 };
