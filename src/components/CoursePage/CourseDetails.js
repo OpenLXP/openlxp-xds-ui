709dfa88e748d093c8acd51c28e0e6d96e6f97fd
rglasgow-deloitte <rglasgow@deloitte.com>
2021-08-23 15:46:43 -0400

    Adds tests for courseDetails.js

--- src/components/CoursePage/CourseDetails.js
+++ src/components/CoursePage/CourseDetails.js
@@ -12,13 +12,18 @@ export default function CourseDetail({ icon, label, value }) {
   const getIconNameToUse = (name) =>
     icons[name] ? icons[name] : icons["calendar"];
 
-  return <div>
-    <div className="flex flex-row items-center text-icon-blue gap-1">
-      <ion-icon name={getIconNameToUse(icon)}/>
-      {label && <h2 className="font-semibold font-sans">{label}</h2>}
+  return (
+    <div>
+      <div className="flex flex-row items-center text-icon-blue gap-1">
+        <ion-icon
+          name={getIconNameToUse(icon)}
+          data-testid={getIconNameToUse(icon)}
+        />
+        {label && <h2 className="font-semibold font-sans">{label}</h2>}
+      </div>
+      <div className="border rounded-md px-2 py-1 text-sm min-w-full">
+        {value === "" || !value ? "N/A" : value}
+      </div>
     </div>
-    <div className="border rounded-md px-2 py-1 text-sm min-w-full">
-      {value === "" || !value ? "N/A" : value}
-    </div>
-  </div>
+  );
 }
