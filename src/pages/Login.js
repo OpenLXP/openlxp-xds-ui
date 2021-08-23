9dc779133985bb40c618e07cbec93a0981f8e061
schittlur <78377530+schittlur@users.noreply.github.com>
2021-08-23 14:18:48 -0400

    removed double imports

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -8,7 +8,6 @@ import {useState} from "react";
 
 import PageWrapper from "../components/common/PageWrapper";
 import {ErrorText, Title, Link} from "../components/common/text/text";
-import {Button} from "../components/common/button/buttons";
 import {InputField} from "../components/common/input/inputs";
 import {loginUser} from "../store/user";
 import {Button} from "../components/common/button/buttons";
