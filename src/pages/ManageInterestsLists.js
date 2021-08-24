30f377634bb2d300c1fc05b167344fe0257cf217
rglasgow-deloitte <rglasgow@deloitte.com>
2021-08-24 10:34:33 -0400

    Adds tests for manage interest list. Removes user check due to route level permissions

--- src/pages/ManageInterestsLists.js
+++ src/pages/ManageInterestsLists.js
@@ -1,44 +1,36 @@
-import {useEffect} from "react";
-import {useDispatch, useSelector} from "react-redux";
-import {useHistory} from "react-router-dom";
-import {Error, Loading} from "../components/common/messages/messages";
-import {Title} from "../components/common/text/text";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { Error, Loading } from "../components/common/messages/messages";
+import { Title } from "../components/common/text/text";
 
-import {getUserLists} from "../store/lists"
+import { getUserLists } from "../store/lists";
 import PageWrapper from "../components/common/PageWrapper";
-import InterestList
-  from "../components/ManageInterestLists/InterestList/InterestList";
+import InterestList from "../components/ManageInterestLists/InterestList/InterestList";
 
-const ManageInterestLists = () => {
+export default function ManageInterestLists() {
   const dispatch = useDispatch();
   const history = useHistory();
   const { user } = useSelector((state) => state.user);
   const { lists, status } = useSelector((state) => state.lists);
 
-  // handles the initial load if a user does not exist
-  useEffect(() => {
-    if (!user) history.push("/signin");
-  }, [user]);
-
   // handles the state of the lists
   useEffect(() => {
     // updates the redux state with an array of lists.
     dispatch(getUserLists(user?.token));
-  }, [dispatch, user.token]);
+  }, [user.token]);
 
   return (
     <PageWrapper className={"my-5 bg-body-gray"}>
-      <Title title={"Manage Interest Lists"}/>
+      <Title title={"Manage Interest Lists"} />
       <div className="my-2 p-2 rounded-md bg-white">
-        {status === "succeeded" && lists?.map((list) => {
-          return <InterestList list={list}/>;
-        })}
-        {status === "loading" && <Loading/>}
-        {status === "rejected" &&
-        <Error error={"Contact a system administrator."}/>}
+        {status === "loading" && <Loading />}
+        {status === "failed" && (
+          <Error error={"Contact a system administrator."} />
+        )}
+        {status === "succeeded" &&
+          lists?.map((list) => <InterestList list={list} />)}
       </div>
     </PageWrapper>
   );
-};
-
-export default ManageInterestLists;
+}
