4318ac7584ae5fcaea538b7b3f4f6f55fb414c28
rglasgow-deloitte <rglasgow@deloitte.com>
2021-09-23 11:59:58 -0400

    Updates the experience card to use metadata_key_hash

--- src/components/ExperienceCard/ExperienceCard.js
+++ src/components/ExperienceCard/ExperienceCard.js
@@ -17,17 +17,13 @@ const ExperienceCard = (props) => {
   };
 
   // if configuration is loaded and there is an image fallback, use that
-  if (configuration && configuration.course_img_fallback) {
+  if (configuration?.course_img_fallback) {
     courseThumbnail = backendHost + configuration.course_img_fallback;
     style = null;
   }
 
   // override the default img if one is passed in
-  if (
-    props.courseObj &&
-    props.courseObj.Technical_Information &&
-    props.courseObj.Technical_Information.Thumbnail
-  ) {
+  if (props.courseObj?.Technical_Information?.Thumbnail) {
     courseThumbnail = props.courseObj.Technical_Information.Thumbnail;
     style = null;
   }
@@ -64,7 +60,10 @@ const ExperienceCard = (props) => {
             <b>
               <Link
                 to={{
-                  pathname: "/course/" + props.courseObj.meta?.id,
+                  pathname:
+                    "/course/" +
+                    (props.courseObj?.meta?.metadata_key_hash ||
+                      props.courseObj.meta?.id),
                   state: {
                     expObj: props.courseObj,
                     imgLink: courseThumbnail,
