69d8e9a6c3d5e87895179690009c7072e11891f0
rglasgow-deloitte <rglasgow@deloitte.com>
2021-10-15 13:23:00 -0400

    updates default layout to get config

--- components/layouts/DefaultLayout.js
+++ components/layouts/DefaultLayout.js
@@ -1,13 +1,14 @@
-import Header from "../Header";
+import useConfig from "../../hooks/useConfig";
+import Header from '../Header';
+
 
 export default function DefaultLayout({ children }) {
+  const { data } = useConfig();
   return (
-    <div className={"relative min-h-screen"}>
-      <Header/>
-      <div className={"max-w-7xl mx-auto px-4 sm:px-4 lg:px-8"}>
-        {children}
-      </div>
-      <div className={"absolute bottom-0"}>test</div>
+    <div className={'relative min-h-screen'}>
+      <Header />
+      <div className={'max-w-7xl mx-auto px-4 sm:px-4 lg:px-8'}>{children}</div>
+      <div className={'absolute bottom-0'}>test</div>
     </div>
   );
 }
