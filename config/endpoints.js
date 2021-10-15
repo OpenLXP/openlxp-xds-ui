978c5ec18c61af24464b71722644c4ed465a76e6
rglasgow-deloitte <rglasgow@deloitte.com>
2021-10-15 09:16:38 -0400

    Adds more enpoints

--- config/endpoints.js
+++ config/endpoints.js
@@ -1,12 +1,16 @@
 // local host endpoint from .env
-const backendHost = process.env.NEXT_PUBLIC_BACKEND_HOST
-const api = "api/"
+const backendHost = process.env.NEXT_PUBLIC_BACKEND_HOST;
+const api = 'api/';
+const elasticApi = 'es-api/';
 
 // authentication urls
-export const authLogin = `${backendHost}${api}auth/login`
-export const authRegister = `${backendHost}${api}auth/register`
-export const authLogout = `${backendHost}${api}auth/logout`
+export const authLogin = `${backendHost}${api}auth/login`;
+export const authRegister = `${backendHost}${api}auth/register`;
+export const authLogout = `${backendHost}${api}auth/logout`;
 
 
 //
-export const interestLists = `${backendHost}${api}interest-lists/`
+export const interestLists = `${backendHost}${api}interest-lists/`;
+
+// search url
+export const searchUrl = `${backendHost}${elasticApi}`;
