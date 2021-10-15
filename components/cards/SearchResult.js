0e93f3bd0efdef87f92c81d40ee02d8b7c6a044a
rglasgow-deloitte <rglasgow@deloitte.com>
2021-10-15 13:21:56 -0400

    Updates search result to have action buttons

--- components/cards/SearchResult.js
+++ components/cards/SearchResult.js
@@ -1,15 +1,37 @@
+import { useAuth } from '../../contexts/AuthContext';
+import Link from 'next/link';
+import SaveBtn from '../buttons/SaveBtn';
+import ShareBtn from '../buttons/ShareBtn';
+import ViewBtn from '../buttons/ViewBtn';
+
 export default function SearchResult({ result }) {
-  const {
-    CourseTitle,
-    CourseShortDescription,
-    CourseProviderName
-  } = result.Course;
+  const { user } = useAuth();
+  const { CourseTitle, CourseShortDescription, CourseProviderName } =
+    result.Course;
+  const { id } = result.meta;
 
   return (
     <div>
-      <h1 className={'text-xl font-semibold'}>{CourseTitle}</h1>
-      <h2 className={'font-normal font-sans'}><span
-        className={'font-semibold'}>Provider:&nbsp;</span>{CourseProviderName}
+      <div className='inline-flex gap-2 justify-between items-center w-full'>
+        <Link href={'/course/' + id}>
+          <h1
+            className={
+              'text-xl font-semibold line-clamp-1 hover:underline hover:text-blue-400 cursor-pointer hover:text-shadow'
+            }
+            title={CourseTitle}
+          >
+            {CourseTitle}
+          </h1>
+        </Link>
+        <div className='inline-flex gap-2'>
+          <ViewBtn id={id} />
+          <ShareBtn id={id} />
+          {user && <SaveBtn id={id} />}
+        </div>
+      </div>
+      <h2 className={'font-normal font-sans'}>
+        <span className={'font-semibold'}>Provider:&nbsp;</span>
+        {CourseProviderName}
       </h2>
       <p className={'line-clamp-3'}>{CourseShortDescription}</p>
     </div>
