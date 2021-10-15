44fb64155b7be7481066908fc1a3bb7ecc3eef02
rglasgow-deloitte <rglasgow@deloitte.com>
2021-10-15 13:23:18 -0400

    Comments out button

--- pages/index.js
+++ pages/index.js
@@ -40,13 +40,13 @@ export default function Home() {
           onChange={setParameters}
         />
       </div>
-      <button
+      {/* <button
         id={'search-button'}
         onClick={handleSearch}
         className={'flex justify-center items-center gap-2 text-blue-400 rounded-md hover:shadow-md bg-blue-50 hover:bg-blue-400 hover:text-white px-4 py-2 transform transition-all duration-150 ease-in-out border-blue-400 border-2 outline-none focus:ring-2 ring-blue-400'}
       ><SearchIcon className={'h-5 w-5'} />
         Search
-      </button>
+      </button> */}
     </div>
   </DefaultLayout>;
 }
