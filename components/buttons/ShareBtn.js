7f5def0d2b36f2b8d04f223b906959875edddb22
rglasgow-deloitte <rglasgow@deloitte.com>
2021-10-18 16:29:07 -0400

    updates show to use new timeout structure

--- components/buttons/ShareBtn.js
+++ components/buttons/ShareBtn.js
@@ -1,9 +1,8 @@
 import { ShareIcon } from '@heroicons/react/solid';
 import useTimeout from '../../hooks/useTimeout';
-import useWindow from '../../hooks/useWindow';
 
 export default function ShareBtn({ id }) {
-  const [show, setShow] = useTimeout(1000);
+  const { view, show } = useTimeout(1000);
   function copyToClipboard() {
     if (typeof window === 'undefined') return;
 
@@ -19,19 +18,19 @@ export default function ShareBtn({ id }) {
       navigator.clipboard.writeText(`${hostName}/course/${id}`);
     }
     // showing the copy message
-    setShow();
+    show();
   }
 
   return (
     <button
-      onClick={ copyToClipboard }
+      onClick={copyToClipboard}
       title='share course'
       className='flex justify-center items-center gap-2 text-blue-400 rounded-full hover:shadow-md bg-blue-50 hover:bg-blue-400 hover:text-white p-1.5 transform transition-all duration-150 ease-in-out border-blue-400 border-2 focus:ring-2 ring-blue-400 outline-none'
     >
       <ShareIcon className='h-5 w-5' />
       <div
         className={`${
-          show ? 'block' : 'invisible'
+          view ? 'block' : 'invisible'
         } absolute -bottom-12 text-white text-xs px-2 py-2 flex items-center justify-center flex-col`}
       >
         <span className='w-4 h-4 bg-blue-400 transform rotate-45 rounded-sm -mb-2 z-10 shadow-md'></span>
