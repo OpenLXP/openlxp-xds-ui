06dbd4b1923128dcf75d8bf02be1f122fefb9e9a
rglasgow-deloitte <rglasgow@deloitte.com>
2021-10-18 17:14:08 -0400

    updates jsconfig, and jest config to handle aliasing

--- jest.config.js
+++ jest.config.js
@@ -17,6 +17,10 @@ module.exports = {
     /* Handle image imports
     https://jestjs.io/docs/webpack#handling-static-assets */
     '^.+\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
+
+    //imports for @ imports
+    '^components/(.*)$': '<rootDir>/components/$1',
+    '^hooks/(.*)$': '<rootDir>/hooks/$1',
   },
   testPathIgnorePatterns: [
     '<rootDir>/node_modules/',
