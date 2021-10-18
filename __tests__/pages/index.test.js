2acff2a2d40fd76db701da6f192a4d6020e40f5c
rglasgow-deloitte <rglasgow@deloitte.com>
2021-10-18 13:57:05 -0400

    updates tests for index.js

--- __tests__/pages/index.test.js
+++ __tests__/pages/index.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, act, fireEvent } from '@testing-library/react';
 import React from 'react';
 import Home from '../../pages/index';
 
@@ -7,8 +7,53 @@ describe('should render the title', () => {
     render(<Home />);
     expect(screen.getByText(`Enterprise Course Catalog`)).toBeInTheDocument();
     expect(screen.getByText(`Department of Defense`)).toBeInTheDocument();
-    expect(screen.getByText(`Search`)).toBeInTheDocument();
-    expect(screen.getByPlaceholderText(`Search the catalog`)).toBeInTheDocument();
+    expect(
+      screen.getByPlaceholderText(`Search the catalog`)
+    ).toBeInTheDocument();
+  });
+
+  it('should not navigate away if no field.keyword or empty keyword', () => {
+    render(<Home />);
+
+    act(() => {
+      fireEvent.change(screen.getByRole('textbox'), {
+        target: { value: '' },
+      });
+    });
+    act(() => {
+      fireEvent.click(screen.getByTitle(/search/i));
+    });
+
+    expect(screen.getByRole('textbox', { id: /search-bar/i }).value).toBe('');
+  });
+
+  it.skip(
+    'should navigate away from the page when a field.keyword is present',
+    () => {
+      render(<Home />);
+
+      act(() => {
+        fireEvent.change(screen.getByPlaceholderText('Search the catalog'), {
+          target: { value: 'updated value' },
+        });
+      });
+
+      act(() => {
+        fireEvent.click(screen.getByTitle(/search/i));
+      });
+    }
+  );
+  it('should update the value of the search bar', () => {
+    render(<Home />);
+
+    act(() => {
+      fireEvent.change(screen.getByPlaceholderText('Search the catalog'), {
+        target: { value: 'updated value' },
+      });
+    });
+
+    expect(screen.getByPlaceholderText('Search the catalog').value).toBe(
+      'updated value'
+    );
   });
 });
-
