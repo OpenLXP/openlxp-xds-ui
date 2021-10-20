0893bfd461ac5f3b60450a7c411615e6c70cdf83
rglasgow-deloitte <rglasgow@deloitte.com>
2021-10-20 09:01:36 -0400

    Updated mock to use component capitalization

--- __mocks__/queryClientMock.js
+++ __mocks__/queryClientMock.js
@@ -1,6 +1,6 @@
 import { QueryClientProvider, QueryClient } from 'react-query';
 
-export const AuthContextWrapper = ({ children }) => {
+export const QueryClientWrapper = ({ children }) => {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>{children} </QueryClientProvider>
