3a081c1be9ede46412920156d1c376575e23cfa2
schittlur <78377530+schittlur@users.noreply.github.com>
2021-10-20 13:40:52 -0400

    Local Storage

--- hooks/useLocalStorage.js
+++ hooks/useLocalStorage.js
@@ -1,8 +1,61 @@
+import { useState, useEffect } from 'react';
 
-export default function getLovalStorageValue(key) {
-    return (window.localStorage.getItem(key));
-}
+export default function useLocalStorage(key, initialValue) {
+  // State to store our value
+  // Pass initial state function to useState so logic is only executed once
+  const getValue = () => {
+    try {
+      // Get from local storage by key
+      const item = window.localStorage.getItem(key);
+      // Parse stored json or if none return initialValue
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      // If error also return initialValue
+      console.log(error);
+      return initialValue;
+    }
+  };
+  const [storedValue, setStoredValue] = useState(getValue);
 
-export default function setLocalStorageValue(key, value) {
-    window.localStorage.setItem(key, JSON.stringify(value));
+  // Return a wrapped version of useState's setter function that ...
+  // ... persists the new value to localStorage.
+  const setValue = (value) => {
+    try {
+      // Allow value to be a function so we have same API as useState
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+      // // Save state
+      // setStoredValue(valueToStore);
+      // Save to local storage
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      // A more advanced implementation would handle the error case
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    setStoredValue(getValue());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    const handleStorageChange = () => {
+      setStoredValue(getValue());
+    };
+
+    // this only works for other documents, not the current one
+    document.addEventListener('storage', handleStorageChange);
+
+    // this is a custom event, triggered in writeValueToLocalStorage
+    document.addEventListener('local-storage', handleStorageChange);
+
+    return () => {
+        document.removeEventListener('storage', handleStorageChange);
+        document.removeEventListener('local-storage', handleStorageChange);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return [storedValue, setValue];
 }
