3f574073799641d74856568699527ca4f8201b35
rglasgow-deloitte <rglasgow@deloitte.com>
2021-10-20 09:02:09 -0400

    updates useTimeout test to cover more

--- __tests__/hooks/useTimeout.test.js
+++ __tests__/hooks/useTimeout.test.js
@@ -1,7 +1,13 @@
 import { renderHook, act } from '@testing-library/react-hooks';
 import useTimeout from '../../hooks/useTimeout';
 
+jest.useFakeTimers();
+jest.spyOn(global, 'setTimeout');
 describe('useTimeout', () => {
+  beforeEach(() => {
+    jest.clearAllTimers();
+  });
+
   it('should default to false', () => {
     const { result } = renderHook(() => useTimeout(1000));
 
@@ -9,25 +15,22 @@ describe('useTimeout', () => {
   });
   it('should show for 1s', () => {
     const { result } = renderHook(() => useTimeout(1000));
-    jest.useFakeTimers();
-    jest.spyOn(global, 'setTimeout');
     act(() => {
       result.current.show();
     });
 
-    expect(setTimeout).toHaveBeenCalledTimes(1);
     expect(setTimeout).toHaveBeenCalledWith(expect.any(Function), 1000);
   });
   it('should show for 1s before returning to false', () => {
     const { result } = renderHook(() => useTimeout(1000));
-    jest.useFakeTimers();
-    jest.spyOn(global, 'setTimeout');
     act(() => {
       result.current.show();
     });
+    act(() => {
+      jest.advanceTimersByTime(10000);
+    });
 
-    expect(setTimeout).toHaveBeenCalledTimes(1);
-    expect( setTimeout ).toHaveBeenCalledWith( expect.any( Function ), 1000 );
-    expect(result.current.state).toBeFalsy()
+    expect(setTimeout).toHaveBeenCalledWith(expect.any(Function), 1000);
+    expect(result.current.state).toBeFalsy();
   });
 });
