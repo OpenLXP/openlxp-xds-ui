27bcca795b02c08edbf404f2bb943f2467f743d7
rglasgow-deloitte <rglasgow@deloitte.com>
2021-10-22 14:16:29 -0400

    adds component

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ import DefaultLayout from '../components/layouts/DefaultLayout';
 import useSpotlightCourses from '../hooks/useSpotlightCourses';
 import { useConfig } from '../hooks/useConfig';
 import { backendHost } from '../config/endpoints';
+import CourseSpotlight from '../components/cards/CourseSpotlight';
 
 export default function Home() {
   const router = useRouter();
@@ -56,29 +57,8 @@ export default function Home() {
           <div className='flex overflow-x-auto px-2 gap-2 py-5 custom-scroll '>
             {spotlight.isSuccess &&
               spotlight.data.length > 0 &&
-              spotlight.data.map(({ Course, meta }) => {
-                return (
-                  <div
-                    className='z-0 overflow-hidden relative rounded-md shadow hover:shadow-lg transform transition-shadow duration-150 ease-in-out bg-blue-200 flex-shrink-0  to-transparent cursor-pointer'
-                    style={{ height: '176px', width: '296px' }}
-                  >
-                    <div className='absolute top-0 p-2 font-sans text-gray-50 text-shadow-md z-20'>
-                      <h2 className='line-clamp-2 font-semibold break-words break-normal'>
-                        {Course.CourseTitle}
-                      </h2>
-                      <div>
-                        <label className='font-semibold'>Provider:&nbsp;</label>
-                        {Course.CourseProviderName}
-                      </div>
-                    </div>
-                    <div className='h-full w-full absolute bg-gradient-to-b from-black-70 to-black-10 z-10'></div>
-                    <img
-                      src={backendHost + uiConfig.data.course_img_fallback}
-                      alt=''
-                      className='h-full w-full absolute'
-                    />
-                  </div>
-                );
+              spotlight.data.map((course) => {
+                return <CourseSpotlight course={course} />;
               })}
           </div>
         </div>
