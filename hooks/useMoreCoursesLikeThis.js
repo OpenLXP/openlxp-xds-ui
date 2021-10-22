7ba6e5120a8cb0a5480ecb8ea518cd3df50414fa
rglasgow-deloitte <rglasgow@deloitte.com>
2021-10-22 14:19:12 -0400

    Adds hook to get more courses like this

---
+++ hooks/useMoreCoursesLikeThis.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import { moreLikeThisUrl } from '../config/endpoints';
+import { useQuery, useQueryClient } from 'react-query';
+import { oneHour } from '../config/timeConstants';
+
+const getMoreCoursesLikeThis = (id) => {
+  return () => axios.get(`${moreLikeThisUrl}${id}/`).then((res) => res.data);
+};
+
+export default function useMoreCoursesLikeThis(id) {
+  const queryClient = useQueryClient();
+  return useQuery(['more-like-this', id], getMoreCoursesLikeThis(id), {
+    onSuccess: (data) => {
+      data?.hits?.map((course) => {
+        queryClient.setQueryData(
+          ['course', course.meta.id],
+          course
+        );
+        queryClient.setQueryDefaults(['course', course.meta.id], {
+          staleTime: oneHour,
+          cacheTime: oneHour,
+        });
+      });
+    },
+  });
+}
