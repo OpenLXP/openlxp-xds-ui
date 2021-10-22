c01c434b3d0af7569f7b37207fb82eb5e7f72b7f
rglasgow-deloitte <rglasgow@deloitte.com>
2021-10-22 08:36:33 -0400

    increased the size of the icon

--- components/buttons/SaveBtn.js
+++ components/buttons/SaveBtn.js
@@ -7,7 +7,7 @@ export default function SaveBtn({ id }) {
       className='inline-flex justify-center items-center gap-2 text-blue-400 rounded-l-lg rounded-r-3xl hover:shadow-md bg-blue-50 hover:bg-blue-400 hover:text-white py-1 pl-1 font-medium pr-2 transform transition-all duration-150 ease-in-out border-blue-400 border-2 focus:ring-2 ring-blue-400 outline-none'
       title='save course'
     >
-      <PlusCircleIcon className='h-5 w-5' />
+      <PlusCircleIcon className='h-6 w-6' />
       Save
     </button>
   );
