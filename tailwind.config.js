b35ea170406888cc5d151b2061c859397b6ea4ea
rglasgow-deloitte <rglasgow@deloitte.com>
2021-10-22 08:39:17 -0400

    adds custom styling to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,12 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      colors: {
+        'black-70': 'rgba(17, 24, 39, .7)',
+        'black-10': 'rgba(17, 24, 39, .1)',
+      },
       boxShadow: {
-        'inner-sm': 'inset 1px 1px 3px rgba(0,0,0,0.2)',
+        'inner-sm': 'inset 1px 1px 3px rgba(17, 24, 39, 0.2)',
       },
     },
   },
