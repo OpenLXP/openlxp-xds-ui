a385426d1798bc99b2ab3143b05dfb1f5c0500d9
rglasgow-deloitte <rglasgow@deloitte.com>
2021-10-26 09:34:03 -0400

    Removes commented code

--- contexts/AuthContext.js
+++ contexts/AuthContext.js
@@ -16,91 +16,35 @@ export function AuthProvider({ children }) {
 
   // Register user
   const register = (userData) => {
-    // setUser(userData);
     setError(null);
     setLocal(userData);
-    // if (typeof window !== 'undefined') {
-    // window.localStorage.setItem('user', JSON.stringify(userData));
-    // }
-
-    // setIsLoading(true);
-    // axios
-    //   .post(authRegister, credentials)
-    //   .then((res) => {
-    //     setUser(res.data);
-    //     if (typeof window !== 'undefined') {
-    //       window.localStorage.setItem('user', JSON.stringify(res.data));
-    //     }
-    //     setIsLoading(false);
-    //     router.push('/');
-    //   })
-    //   .catch((error) => {
-    //     setUser({});
-    //     setError(error);
-    //     setIsLoading(false);
-    //   });
   };
 
   // Login user
   const login = (userData) => {
-    // setUser(userData);
     setError(null);
     setLocal(userData);
-    // if (typeof window !== 'undefined') {
-    //   window.localStorage.setItem('user', JSON.stringify(userData));
-    // }
-    // setIsLoading(true);
-    // axios
-    //   .post(authLogin, { username, password })
-    //   .then((res) => {
-    //     setUser(res.data);
-    //     if (typeof window !== 'undefined') {
-    //       window.localStorage.setItem('user', JSON.stringify(res.data));
-    //     }
-    //     setIsLoading(false);
-    //     router.push('/');
-    //   })
-    //   .catch((error) => {
-    //     setUser({});
-    //     setError(error);
-    //     setIsLoading(false);
-    //   });
   };
 
   // Logout user
   const logout = async () => {
-    // if (typeof window !== 'undefined') {
-    //   window.localStorage.removeItem('user');
-    //   // setUser(null);
-    // }
     removeLocal();
   };
 
   // // Check if user is logged in
-  // const checkUserLoggedIn = async () => {
-  //   if (typeof window !== 'undefined') {
-  //     const jsonData = window.localStorage.getItem('user2');
-
-  //     // if there is local user data
-  //     if (jsonData) {
-  //       const userData = JSON.parse(jsonData);
-  //       // verify the token works
-  //       axios
-  //         .get(interestLists + 'owned', {
-  //           headers: { Authorization: 'Token ' + userData.token },
-  //         })
-  //         .then((res) => {
-  //           setUser(JSON.parse(jsonData));
-  //           setLocal(jsonData)
-  //         })
-  //         .catch((error) => {
-  //           // setUser( null );
-  //           removeLocal()
-  //           router.push('/');
-  //         });
-  //     } else removeLocal();
-  //   }
-  // };
+  const checkUserLoggedIn = async () => {
+    if (typeof window !== 'undefined') {
+      axios
+        .get(interestLists + 'owned', {
+          headers: { Authorization: 'Token ' + userData.token },
+        })
+        .then((res) => {})
+        .catch((error) => {
+          removeLocal();
+          router.push('/');
+        });
+    }
+  };
 
   return (
     <AuthContext.Provider value={{ user, error, register, login, logout }}>
