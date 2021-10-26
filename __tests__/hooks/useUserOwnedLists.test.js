d5f8fdb44d1f5b11fbc1598abfd880e98782690d
rglasgow-deloitte <rglasgow@deloitte.com>
2021-10-26 16:15:42 -0400

    Adds test and data for the test

---
+++ __tests__/hooks/useUserOwnedLists.test.js
@@ -0,0 +1,23 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import mockAxios from 'jest-mock-axios';
+
+import useUserOwnedLists from '../../hooks/useUserOwnedLists';
+import userOwnedListsData from '../../__mocks__/data/userOwnedLists.data';
+
+const queryClient = new QueryClient();
+const wrapper = ({ children }) => (
+  <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+);
+
+describe('useUserOwnedLists', () => {
+  it('should return a list of courses found', async () => {
+    mockAxios.get.mockResolvedValue({ data: userOwnedListsData });
+    const { result, waitForNextUpdate } = renderHook(
+      () => useUserOwnedLists('testToken'),
+      { wrapper }
+    );
+    await waitForNextUpdate(result.current.isSuccess);
+    expect(result.current.data).toEqual(userOwnedListsData);
+  });
+});
