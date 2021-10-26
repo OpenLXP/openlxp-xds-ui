f75320670f65315dea7f093b1cbe259b04e277fb
rglasgow-deloitte <rglasgow@deloitte.com>
2021-10-26 09:29:59 -0400

    Updates styling for buttons

--- components/buttons/SaveBtn.js
+++ components/buttons/SaveBtn.js
@@ -1,6 +1,6 @@
 import { PlusCircleIcon } from '@heroicons/react/outline';
 import { useState } from 'react';
-import SaveModal from '../cards/SaveModal';
+import SaveModal from '../modals/SaveModal';
 
 export default function SaveBtn({ id }) {
   const [showModal, setShowModal] = useState(false);
@@ -8,7 +8,7 @@ export default function SaveBtn({ id }) {
   return (
     <>
       <button
-        className='inline-flex justify-center items-center gap-2 text-blue-400 rounded-l-lg rounded-r-3xl hover:shadow-md bg-blue-50 hover:bg-blue-400 hover:text-white py-1 pl-1 font-medium pr-2 transform transition-all duration-150 ease-in-out border-blue-400 border-2 focus:ring-2 ring-blue-400 outline-none'
+        className='inline-flex justify-center items-center gap-2 text-blue-400 rounded-r-lg rounded-l-3xl hover:shadow-md bg-blue-50 hover:bg-blue-400 hover:text-white py-1 pl-1 font-medium pr-2 transform transition-all duration-150 ease-in-out border-blue-400 border-2 focus:ring-2 ring-blue-400 outline-none'
         title='save course'
         onClick={() => setShowModal(!showModal)}
       >
