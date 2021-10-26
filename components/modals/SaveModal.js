3e38213aced17019fda651b0e586339a900c44b4
rglasgow-deloitte <rglasgow@deloitte.com>
2021-10-26 09:30:26 -0400

    Moves modal to its own folder

--- components/modals/SaveModal.js
+++ components/modals/SaveModal.js
@@ -1,9 +1,21 @@
 import { Dialog, Transition } from '@headlessui/react';
 import { useAuth } from '../../contexts/AuthContext';
-import { Fragment } from 'react';
+import { Fragment, useState, useEffect } from 'react';
+import useUserOwnedLists from '../../hooks/useUserOwnedLists';
+import UserListResult from '../cards/UserListEditResult';
+import { BookOpenIcon, UsersIcon } from '@heroicons/react/outline';
+import useUpdateUserList from '../../hooks/useUpdateUserList';
 
 export default function SaveModal({ id, modalState, closeModal }) {
   const { user } = useAuth();
+  const lists = useUserOwnedLists(user?.token);
+  const mutation = useUpdateUserList(user?.token);
+
+  const [currentList, setCurrentList] = useState([]);
+  useEffect(() => {
+    setCurrentList(lists.data);
+  }, [lists.data]);
+  const handleUpdate = () => {};
 
   return (
     <Transition appear show={modalState} as={Fragment}>
@@ -47,26 +59,30 @@ export default function SaveModal({ id, modalState, closeModal }) {
                 as='h3'
                 className='text-lg font-medium leading-6 text-gray-900'
               >
-                Save Course
+                Your Lists
               </Dialog.Title>
               <div className='mt-2'>
                 <p className='text-sm text-gray-500'>
-                  This course will be saved for later.
+                  Add this course to a list.
                 </p>
                 <p className='text-sm text-gray-500'>Course ID: {id}</p>
               </div>
-
+              <div className='h-72 overflow-y-auto overflow-x-hidden custom-scroll px-1 bg-gray-50 rounded text-left'>
+                {currentList?.map((list) => {
+                  return <pre>{JSON.stringify(list, null, 2)}</pre>;
+                })}
+              </div>
               <div className='mt-4'>
                 <button
                   type='button'
-                  className='m-2 inline-flex text-blue-400 rounded hover:shadow-md bg-blue-50 hover:bg-blue-400 hover:text-white py-1 pl-1 font-medium pr-2 transform transition-all duration-150 ease-in-out border-blue-400 border-2 focus:ring-2 ring-blue-400 outline-none'
+                  className='m-2 inline-flex text-blue-400 rounded hover:shadow-md bg-blue-50 hover:bg-blue-400 hover:text-white py-1 p-2 font-medium transform transition-all duration-150 ease-in-out border-blue-400 border-2 focus:ring-2 ring-blue-400 outline-none'
                   onClick={closeModal}
                 >
                   Save
                 </button>
                 <button
                   type='button'
-                  className='m-2 inline-flex text-blue-400 rounded hover:shadow-md bg-blue-50 hover:bg-blue-400 hover:text-white py-1 pl-1 font-medium pr-2 transform transition-all duration-150 ease-in-out border-blue-400 border-2 focus:ring-2 ring-blue-400 outline-none'
+                  className='m-2 inline-flex text-blue-400 rounded hover:shadow-md bg-blue-50 hover:bg-blue-400 hover:text-white py-1 p-2 font-medium transform transition-all duration-150 ease-in-out border-blue-400 border-2 focus:ring-2 ring-blue-400 outline-none'
                   onClick={closeModal}
                 >
                   Cancel
@@ -77,6 +93,5 @@ export default function SaveModal({ id, modalState, closeModal }) {
         </div>
       </Dialog>
     </Transition>
-
   );
 }
