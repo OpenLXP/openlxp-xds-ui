4ef0b462f0b47a908367eb5cbb9fbdf03fcfa275
rglasgow-deloitte <rglasgow@deloitte.com>
2021-10-26 09:44:21 -0400

    Updates formatting

--- hooks/useUserList.js
+++ hooks/useUserList.js
@@ -1,9 +1,9 @@
 import { interestLists } from '../config/endpoints';
-import { useQuery } from "react-query";
-import axios from "axios";
+import { useQuery } from 'react-query';
+import axios from 'axios';
 
 const getUserList = (id) => {
-  return ()=>axios.get(interestLists + id).then((res) => res.data);
+  return () => axios.get(interestLists + id).then((res) => res.data);
 };
 
 export default function useUserList(id) {
