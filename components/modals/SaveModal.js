43bd6661b9af8b8a719bee8d4b8b127db149b26f
rglasgow-deloitte <rglasgow@deloitte.com>
2021-10-28 14:08:27 -0400

    removes save button, replaced by savemodal

--- components/modals/SaveModal.js
+++ components/modals/SaveModal.js
@@ -1,97 +1,142 @@
 import { Dialog, Transition } from '@headlessui/react';
+import { Fragment, useState, useMemo } from 'react';
 import { useAuth } from '../../contexts/AuthContext';
-import { Fragment, useState, useEffect } from 'react';
+import { useUpdateUserList } from '../../hooks/useUpdateUserList';
 import { useUserOwnedLists } from '../../hooks/useUserOwnedLists';
-import UserListResult from '../cards/UserListEditResult';
-import { BookOpenIcon, UsersIcon } from '@heroicons/react/outline';
-import useUpdateUserList from '../../hooks/useUpdateUserList';
+import { PlusCircleIcon } from '@heroicons/react/outline';
 
-export default function SaveModal({ id, modalState, closeModal }) {
+export default function SaveModal({ courseId }) {
+  // authentication
   const { user } = useAuth();
-  const lists = useUserOwnedLists(user?.token);
-  const mutation = useUpdateUserList(user?.token);
 
-  const [currentList, setCurrentList] = useState([]);
-  useEffect(() => {
-    setCurrentList(lists.data);
-  }, [lists.data]);
-  const handleUpdate = () => {};
+  // user lists
+  const { data: userLists, isSuccess } = useUserOwnedLists(user?.token);
+  const { mutate } = useUpdateUserList(user?.token);
+
+  // add a course to the selected list
+  const addCourseToList = (listId) => {
+    const listData = userLists.find((list) => list.id === listId);
+    listData.experiences.push(courseId);
+    mutate({ listData: listData, id: listId });
+  };
+
+  // remove a course from the selected list
+  const removeCourseFromList = (listId) => {
+    const listData = userLists.find((list) => list.id === listId);
+    const modified = {
+      name: listData.name,
+      description: listData.description,
+      experiences: listData.experiences.filter((exp) => exp !== courseId),
+    };
+    mutate({ listData: modified, id: listId });
+  };
+
+  // modal states
+  let [isOpen, setIsOpen] = useState(false);
+  const closeModal = () => setIsOpen(false);
+  const openModal = () => setIsOpen(true);
 
   return (
-    <Transition appear show={modalState} as={Fragment}>
-      <Dialog
-        as='div'
-        className='fixed inset-0 z-10 overflow-y-auto'
-        onClose={closeModal}
+    <>
+      <button
+        type='button'
+        onClick={openModal}
+        className='inline-flex justify-center items-center gap-2 text-blue-400 rounded-r-lg rounded-l-3xl hover:shadow-md bg-blue-50 hover:bg-blue-400 hover:text-white py-1 pl-1 font-medium pr-2 transform transition-all duration-150 ease-in-out border-blue-400 border-2 focus:ring-2 ring-blue-400 outline-none'
       >
-        <Dialog.Overlay className='fixed inset-0 bg-black opacity-20' />
+        <PlusCircleIcon className='h-6 w-6' />
+        Save
+      </button>
 
-        <div className='min-h-screen px-4 text-center'>
-          <Transition.Child
-            as={Fragment}
-            enter='ease-out duration-300'
-            enterFrom='opacity-0'
-            enterTo='opacity-100'
-            leave='ease-in duration-200'
-            leaveFrom='opacity-100'
-            leaveTo='opacity-0'
-          >
-            <Dialog.Overlay className='fixed inset-0' />
-          </Transition.Child>
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog
+          as='div'
+          className='fixed inset-0 z-10 overflow-y-auto'
+          onClose={closeModal}
+        >
+          <div className='min-h-screen text-center'>
+            <Transition.Child
+              as={Fragment}
+              enter='ease-out duration-200'
+              enterFrom='opacity-0'
+              enterTo='opacity-100'
+              leave='ease-in duration-100'
+              leaveFrom='opacity-100'
+              leaveTo='opacity-0'
+            >
+              <Dialog.Overlay className='fixed inset-0 bg-gray-700 bg-opacity-10' />
+            </Transition.Child>
 
-          <span
-            className='inline-block h-screen align-middle'
-            aria-hidden='true'
-          >
-            &#8203;
-          </span>
-          <Transition.Child
-            as={Fragment}
-            enter='ease-out duration-300'
-            enterFrom='opacity-0 scale-95'
-            enterTo='opacity-100 scale-100'
-            leave='ease-in duration-200'
-            leaveFrom='opacity-100 scale-100'
-            leaveTo='opacity-0 scale-95'
-          >
-            <div className='inline-block w-full max-w-md p-6 my-8 overflow-hidden align-middle transition-all transform bg-white shadow-xl rounded-2xl'>
-              <Dialog.Title
-                as='h3'
-                className='text-lg font-medium leading-6 text-gray-900'
-              >
-                Your Lists
-              </Dialog.Title>
-              <div className='mt-2'>
-                <p className='text-sm text-gray-500'>
-                  Add this course to a list.
-                </p>
-                <p className='text-sm text-gray-500'>Course ID: {id}</p>
-              </div>
-              <div className='h-72 overflow-y-auto overflow-x-hidden custom-scroll px-1 bg-gray-50 rounded text-left'>
-                {currentList?.map((list) => {
-                  return <pre>{JSON.stringify(list, null, 2)}</pre>;
-                })}
-              </div>
-              <div className='mt-4'>
-                <button
-                  type='button'
-                  className='m-2 inline-flex text-blue-400 rounded hover:shadow-md bg-blue-50 hover:bg-blue-400 hover:text-white py-1 p-2 font-medium transform transition-all duration-150 ease-in-out border-blue-400 border-2 focus:ring-2 ring-blue-400 outline-none'
-                  onClick={closeModal}
+            {/* This element is to trick the browser into centering the modal contents. */}
+            <span
+              className='inline-block h-screen align-middle'
+              aria-hidden='true'
+            >
+              &#8203;
+            </span>
+            <Transition.Child
+              as={Fragment}
+              enter='ease-out duration-200'
+              enterFrom='opacity-0 scale-95'
+              enterTo='opacity-100 scale-100'
+              leave='ease-in duration-100'
+              leaveFrom='opacity-100 scale-100'
+              leaveTo='opacity-0 scale-95'
+            >
+              <div className='inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl'>
+                <Dialog.Title
+                  as='h3'
+                  className='text-lg font-medium leading-6 text-gray-900'
                 >
-                  Save
-                </button>
-                <button
-                  type='button'
-                  className='m-2 inline-flex text-blue-400 rounded hover:shadow-md bg-blue-50 hover:bg-blue-400 hover:text-white py-1 p-2 font-medium transform transition-all duration-150 ease-in-out border-blue-400 border-2 focus:ring-2 ring-blue-400 outline-none'
-                  onClick={closeModal}
-                >
-                  Cancel
-                </button>
+                  Add course to lists
+                </Dialog.Title>
+                <div className='mt-2 w-full py-2 px-0.5 rounded-md overflow-y-auto h-56 custom-scroll border bg-gray-50 space-y-1'>
+                  {isSuccess &&
+                    userLists?.map((list) => {
+                      const contained = list.experiences.includes(courseId);
+                      return (
+                        <div
+                          className={` inline-flex justify-between w-full bg-white rounded-md py-2 px-1 border`}
+                        >
+                          {list.name}
+
+                          {contained ? (
+                            <button
+                              className='bg-red-50 px-2 rounded border border-red-500 text-red-600 hover:bg-red-500 hover:text-white transform transition-colors duration-100 ease-in-out'
+                              onClick={() => {
+                                removeCourseFromList(list.id);
+                              }}
+                            >
+                              Remove
+                            </button>
+                          ) : (
+                            <button
+                              className='bg-green-50 px-2 rounded border border-green-500 text-green-600 hover:bg-green-500 hover:text-white transform transition-colors duration-100 ease-in-out'
+                              onClick={() => {
+                                addCourseToList(list.id);
+                              }}
+                            >
+                              Add
+                            </button>
+                          )}
+                        </div>
+                      );
+                    })}
+                </div>
+
+                <div className='mt-4'>
+                  <button
+                    type='button'
+                    className='inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500'
+                    onClick={closeModal}
+                  >
+                    Got it, thanks!
+                  </button>
+                </div>
               </div>
-            </div>
-          </Transition.Child>
-        </div>
-      </Dialog>
-    </Transition>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
   );
 }
