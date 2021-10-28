813537d98870e807f5957da50fd9e969a32bfac9
rglasgow-deloitte <rglasgow@deloitte.com>
2021-10-28 14:06:20 -0400

    updates data

--- __mocks__/data/course.data.js
+++ __mocks__/data/course.data.js
@@ -4,9 +4,10 @@ export default {
     CourseShortDescription: 'Short Description',
     CourseURL: 'CourseURL',
     CourseCode: '1234',
-    CourseProviderName:'Provider Name'
+    CourseProviderName: 'Provider Name',
   },
   meta: {
-    id:'123456'
-  }
+    id: '123456',
+    metadata_key_hash: '123456',
+  },
 };
