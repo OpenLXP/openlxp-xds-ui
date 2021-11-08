7e5d169070fb38b84380e7a48495ec495eef24da
rglasgow-deloitte <rglasgow@deloitte.com>
2021-11-08 08:14:25 -0500

    Updates logic to navigate user away from page if not authenticated.

--- pages/lists/savedSearches.js
+++ pages/lists/savedSearches.js
@@ -1,20 +1,24 @@
 import { EyeIcon, TrashIcon } from '@heroicons/react/outline';
-import React from 'react';
+import React, { useEffect } from 'react';
 import DefaultLayout from '../../components/layouts/DefaultLayout';
 import { useAuth } from '../../contexts/AuthContext';
 import { useSaveSearchList } from '../../hooks/useSaveSearch';
 import { useDeleteSavedSearch } from '../../hooks/useDeleteSavedSearch';
 import Link from 'next/link';
-import { backendHost } from '../../config/endpoints';
+import { useRouter } from 'next/router';
 
 export default function SavedSearches() {
   const { user } = useAuth();
-  const { data, isSuccess } = useSaveSearchList(user?.token);
+  const { data, isSuccess, isLoading, isError } = useSaveSearchList(
+    user?.token
+  );
   const { mutate } = useDeleteSavedSearch(user?.token);
+  const router = useRouter();
 
-  const handleDelete = (id) => {
-    mutate({ id });
-  };
+  // if a user is not logged in
+  useEffect(() => {
+    if (!user) router.push('/');
+  }, []);
 
   return (
     <DefaultLayout footerLocation='absolute'>
@@ -25,15 +29,30 @@ export default function SavedSearches() {
         <div className=' rounded-md overflow-hidden shadow border'>
           <div className='grid grid-cols-8 bg-gray-50 h-12 items-center px-2 font-sans font-semibold'>
             <div className='col-span-3'>Search Title</div>
-            <div className='col-span-3'>Course Provider</div>
+            <div className='col-span-3'>Query</div>
           </div>
           <div className='overflow-y-auto custom-scroll'>
+            {isError && (
+              <div className='text-center text-gray-600 bg-white'>
+                There was an error loading your saved searches
+              </div>
+            )}
+            {isLoading && (
+              <div className='text-center text-gray-600 bg-white'>
+                Loading...
+              </div>
+            )}
+            {isSuccess && data.length === 0 && (
+              <div className='text-center text-gray-600 bg-white'>
+                You have no saved searches
+              </div>
+            )}
             {isSuccess &&
               data.length > 0 &&
               data?.map((list, index) => {
-                console.log(list);
                 return (
                   <div
+                    key={list.id}
                     className={`${
                       index % 2 === 0 ? 'bg-white' : 'bg-gray-50'
                     } grid grid-cols-8 w-full p-2 items-center`}
@@ -46,18 +65,18 @@ export default function SavedSearches() {
                       <Link href={`${list.query}`}>
                         <button
                           id='view'
-                          className='flex items-center gap-2 bg-gray-50 border-2 text-gray-500 border-gray-500 p-1 rounded-full transform transition-color duration-150 ease-in-out hover:text-white hover:bg-gray-500'
+                          title='view'
+                          className='flex items-center gap-2 bg-gray-50 border text-gray-500 border-gray-500 p-1 rounded-full transform transition-color duration-150 ease-in-out hover:text-white hover:bg-gray-500'
                         >
                           <EyeIcon className='h-5 w-5' />
                         </button>
                       </Link>
 
                       <button
-                        onClick={() => {
-                          mutate({ id: list.id });
-                        }}
+                        onClick={() => mutate({ id: list.id })}
                         id='delete'
-                        className='flex items-center justify-center gap-2 bg-red-50 border-2 border-red-500 text-red-500 rounded-l-3xl rounded-r-md py-1 px-2 hover:bg-red-600 hover:text-white transform transition-all duration-150'
+                        title='delete'
+                        className='flex items-center justify-center gap-2 bg-red-50 border border-red-500 text-red-500 rounded-l-3xl rounded-r-md py-1 px-2 hover:bg-red-600 hover:text-white transform transition-all duration-150'
                       >
                         <TrashIcon className='h-5 w-5' />
                         Delete
