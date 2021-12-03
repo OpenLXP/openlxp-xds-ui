b8eac32a747051c2243845084dce1fc0b6c3e8c5
rglasgow-deloitte <rglasgow@deloitte.com>
2021-12-03 15:45:56 -0500

    Adds template for 401 page.
    Lays out the structure for the searchList.js page and the 401 page.

---
+++ pages/401.js
@@ -0,0 +1,12 @@
+export default function NotAuthorized({ errorMessage }) {
+    
+    // should hang on page for 15 seconds
+    // should show count down to redierect 
+    // should have a button to go home
+    // redirect at end of countdown to home.
+
+
+    return (
+        <div></div>
+    )
+}
