f624b1121f5d7d5f1fc34a57f9c9fa8fd9fadad4
rglasgow-deloitte <rglasgow@deloitte.com>
2022-02-09 09:57:08 -0500

    Update to use min-h-screen

--- src/components/layouts/DefaultLayout.js
+++ src/components/layouts/DefaultLayout.js
@@ -5,7 +5,7 @@ import Header from '../Header';
 export default function DefaultLayout({ children, footerLocation }) {
   const { data } = useConfig();
   return (
-    <div className={'relative custom-scroll'}>
+    <div className={'relative custom-scroll min-h-screen'}>
       <Header />
       <div className='max-w-7xl mx-auto px-4 sm:px-4 lg:px-8'>{children}</div>
       <Footer location={footerLocation} />
