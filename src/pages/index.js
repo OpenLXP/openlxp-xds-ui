89d377d835abab3d6632dfe78ceae4462ca79c04
rglasgow-deloitte <rglasgow@deloitte.com>
2022-02-14 20:22:24 -0500

    updates home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,14 @@
-import { useRouter } from 'next/router';
-import Image from 'next/image';
-import React from 'react';
-
-// components
 import { sendStatement } from '@/utils/xapi/xAPIWrapper';
-import CourseSpotlight from '@/components/cards/CourseSpotlight';
-import DefaultLayout from '@/components/layouts/DefaultLayout';
-import SearchBar from '@/components/inputs/SearchBar';
-import logo from '@/public/logo.png';
-
-// hooks
 import { useAuth } from '@/contexts/AuthContext';
 import { useConfig } from '@/hooks/useConfig';
+import { useRouter } from 'next/router';
+import CourseSpotlight from '@/components/cards/CourseSpotlight';
+import Footer from '@/components/Footer';
+import Header from '@/components/Header';
+import Image from 'next/image';
+import React from 'react';
+import SearchBar from '@/components/inputs/SearchBar';
+import logo from '@/public/logo.png';
 import useField from '@/hooks/useField';
 import useSpotlightCourses from '@/hooks/useSpotlightCourses';
 
@@ -50,41 +47,36 @@ export default function Home() {
   };
 
   return (
-    <DefaultLayout>
-      <div className='flex flex-col items-center justify-center min-h-screen gap-8'>
-        <div className={'flex flex-col mt-10 text-center items-center gap-4'}>
-          <Image src={logo} height={250} width={250} alt='' />
-          <div>
-            <h1 className={'text-3xl font-semibold'}>
-              Enterprise Course Catalog
-            </h1>
-            <h2 className={'text-xl sans'}>Department of Defense</h2>
-          </div>
-        </div>
-        <div className={'w-6/12'}>
-          <SearchBar
-            parameters={fields}
-            onReset={resetKey}
-            onClick={handleSearch}
-            onChange={handleChange}
-          />
-        </div>
-
-        <div className='flex flex-col justify-center w-full mt-44 overflow-x-hidden'>
-          {spotlight.isSuccess && spotlight.data.length > 0 && (
-            <span className={'text-gray-400 italic block pb-2 font-sans'}>
-              Spotlight Courses
-            </span>
-          )}
-          <div className='inline-flex overflow-x-auto gap-2 pb-4 mb-24 custom-scroll '>
-            {spotlight.isSuccess &&
-              spotlight.data.length > 0 &&
-              spotlight.data.map((course) => {
-                return <CourseSpotlight course={course} key={course.meta.id} />;
-              })}
-          </div>
+    <>
+      <Header />
+      <div className='max-w-7xl mx-auto flex flex-col items-center justify-center mt-10'>
+        <Image src={logo} height={250} width={250} alt='' />
+        <h1 className='text-3xl font-bold mt-4'>Enterprise Course Catalog</h1>
+        <h2 className='text-xl font-sans mt-2'>Department of Defense</h2>
+      </div>
+      <div className='w-6/12 mx-auto mt-6'>
+        <SearchBar
+          parameters={fields}
+          onReset={resetKey}
+          onClick={handleSearch}
+          onChange={handleChange}
+        />
+      </div>
+      {spotlight.isSuccess && spotlight.data.length > 0 && (
+        <span className={'text-gray-400 italic block mt-24 font-sans px-2 max-w-7xl mx-auto'}>
+          Spotlight Courses
+        </span>
+      )}
+      <div className='flex flex-col justify-center w-full mt-4 px-2 max-w-7xl mx-auto'>
+        <div className='inline-flex overflow-x-auto gap-2 pb-4 custom-scroll '>
+          {spotlight.isSuccess &&
+            spotlight.data.length > 0 &&
+            spotlight.data.map((course) => {
+              return <CourseSpotlight course={course} key={course.meta.id} />;
+            })}
         </div>
       </div>
-    </DefaultLayout>
+      <Footer />
+    </>
   );
 }
