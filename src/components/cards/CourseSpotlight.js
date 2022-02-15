e7ecd9a505955cc7cee0a04edb8e426c364d0ebe
rglasgow-deloitte <rglasgow@deloitte.com>
2022-02-14 20:20:16 -0500

    updates courseSpotlight

--- src/components/cards/CourseSpotlight.js
+++ src/components/cards/CourseSpotlight.js
@@ -2,22 +2,25 @@ import { backendHost } from '@/config/endpoints';
 import { useConfig } from '@/hooks/useConfig';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
-import React from 'react';
+import React, { useMemo } from 'react';
 
 export default function CourseSpotlight({ course }) {
   const { Course, meta, Technical_Information, Course_Instance } = {
     ...course,
   };
-  const uiConfig = useConfig();
+  const config = useConfig();
   const router = useRouter();
 
-  let thumbnail = null;
+  const thumbnail = useMemo(() => {
+    let image = null;
 
-  if (Technical_Information?.Thumbnail) {
-    thumbnail = Technical_Information.Thumbnail;
-  } else if (uiConfig.isSuccess && uiConfig.data.course_img_fallback) {
-    thumbnail = `${backendHost}${uiConfig.data.course_img_fallback}`;
-  }
+    if (Course_Instance) {
+      image = Course_Instance.Thumbnail;
+    } else {
+      image = `${backendHost}${config.data.course_img_fallback}`;
+    }
+    return image;
+  }, [Course_Instance, config]);
 
   const handleVisitCourse = () => {
     router.push(
@@ -29,7 +32,7 @@ export default function CourseSpotlight({ course }) {
     <Link href={`/course/${meta.metadata_key_hash || meta.id}`}>
       <div
         onClick={handleVisitCourse}
-        className='z-0 overflow-hidden relative rounded-md shadow hover:shadow-lg bg-blue-200 cursor-pointer flex-shrink-0 transform transition-shadow duration-150 ease-in-out'
+        className='z-0 overflow-hidden relative rounded-md shadow-stone-200 hover:shadow-lg bg-stone-200 cursor-pointer flex-shrink-0 transform transition-shadow duration-150 ease-in-out'
         style={{ height: '176px', width: '296px' }}
       >
         <div
@@ -42,10 +45,6 @@ export default function CourseSpotlight({ course }) {
               <span className='font-semibold'>Provider:&nbsp;</span>
               {Course.CourseProviderName}
             </div>
-            <div>
-              <span className='font-semibold'>Start Date:&nbsp;</span>
-              {new Date(Course_Instance?.StartDate).toLocaleDateString()}
-            </div>
           </div>
         </div>
         <div
@@ -53,7 +52,12 @@ export default function CourseSpotlight({ course }) {
           className='h-full w-full absolute bg-gradient-to-b from-black-70 to-black-10 z-10'
         />
         {thumbnail && (
-          <img src={thumbnail} alt='' className='h-full w-full absolute' />
+          // eslint-disable-next-line @next/next/no-img-element
+          <img
+            src={thumbnail}
+            alt=''
+            className='h-8 w-12 absolute bottom-0 right-0 m-2'
+          />
         )}
       </div>
     </Link>
