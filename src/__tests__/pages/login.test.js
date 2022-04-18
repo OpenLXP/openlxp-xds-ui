5c4360f503ef7dbf2dacb4c6641cb388897963e8
rglasgow-deloitte <rglasgow@deloitte.com>
2022-04-18 11:48:40 -0400

    refactors login to use form

--- src/__tests__/pages/login.test.js
+++ src/__tests__/pages/login.test.js
@@ -66,49 +66,18 @@ describe('Login Page', () => {
     });
     expect(MockAxios.post).toHaveBeenCalled();
   });
-
-  
 });
 
 describe('Login Page', () => {
-  // it('should render the Login screen title, input fields, and buttons', () => {
-  //   useAuth.mockImplementation(() => ({
-  //     login: jest.fn(),
-  //     logout: jest.fn(),
-  //   }));
-  //   render(
-  //     <QueryClientWrapper>
-  //       <Login />
-  //     </QueryClientWrapper>
-  //   );
-  //   expect(screen.getByText(/Sign in to your account/i)).toBeInTheDocument();
-  //   expect(screen.getByText(`Create an Account`)).toBeInTheDocument();
-  //   expect(screen.getByText(`Login`)).toBeInTheDocument();
-  // });
-
-  // it.skip('should render the sso button', () => {
-  //   useAuth.mockImplementation(() => ({
-  //     login: jest.fn(),
-  //     logout: jest.fn(),
-  //   }));
-  //   render(
-  //     <QueryClientWrapper>
-  //       <Login />
-  //     </QueryClientWrapper>
-  //   );
-  //   // expect(screen.getByText(`Single Sign On`)).toBeInTheDocument();
-  // });
-
   describe('Actions', () => {
     beforeEach(() => {
-      useAuth.mockImplementation(() => ({
-        login: jest.fn(),
-        logout: jest.fn(),
-      }));
+      useUnauthenticatedUser();
       render(
-        <QueryClientWrapper>
-          <Login />
-        </QueryClientWrapper>
+        <MemoryRouterProvider>
+          <QueryClientWrapper>
+            <Login />
+          </QueryClientWrapper>
+        </MemoryRouterProvider>
       );
     });
 
@@ -124,33 +93,23 @@ describe('Login Page', () => {
 
     it('should change values on input: Password', () => {
       const input = screen.getByPlaceholderText('Password');
-
-      fireEvent.change(input, { target: { value: 'password' } });
+      act(() => {
+        fireEvent.change(input, { target: { value: 'password' } });
+      });
 
       expect(input.value).toBe('password');
     });
 
     it('should change show error message for empty attributes', () => {
       const input = screen.getByPlaceholderText('Password');
+      act(() => {
+        fireEvent.change(input, { target: { value: '' } });
 
-      fireEvent.change(input, { target: { value: '' } });
-
-      const button = screen.getByText(/Login/i);
-      fireEvent.click(button);
+        const button = screen.getByText(/Login/i);
+        fireEvent.click(button);
+      });
 
       expect(screen.getByText(/All fields required/i)).toBeInTheDocument();
-    });
-
-    it('should change show error message for valid email', () => {
-      const email = screen.getByPlaceholderText('Email');
-      const password = screen.getByPlaceholderText('Password');
-
-      fireEvent.change(email, { target: { value: 'email' } });
-      fireEvent.change(password, { target: { value: 'password' } });
-      const button = screen.getByText(/Login/i);
-      fireEvent.click(button);
-
-      expect(screen.getByText(/Please enter a valid email address/i));
     });
 
     it('should log a user in.', () => {
@@ -158,13 +117,17 @@ describe('Login Page', () => {
         Promise.resolve({ data: { user: {} } })
       );
 
-      const email = screen.getByPlaceholderText('Email');
-      const password = screen.getByPlaceholderText('Password');
-      fireEvent.change(email, { target: { value: 'email@test.com' } });
-      fireEvent.change(password, { target: { value: 'password' } });
+      act(() => {
+        const email = screen.getByPlaceholderText('Email');
+        const password = screen.getByPlaceholderText('Password');
 
-      const button = screen.getByText(/Login/i);
-      fireEvent.click(button);
+        fireEvent.change(email, { target: { value: 'email@test.com' } });
+        fireEvent.change(password, { target: { value: 'password' } });
+
+        const button = screen.getByText(/Login/i);
+
+        fireEvent.click(button);
+      });
       expect(MockAxios.post).toHaveBeenCalled();
     });
     it('should call the login api', async () => {
@@ -177,13 +140,16 @@ describe('Login Page', () => {
       act(() => {
         fireEvent.change(email, { target: { value: 'email@test.com' } });
         fireEvent.change(password, { target: { value: 'password' } });
-      });
 
-      const button = screen.getByText(/Login/i);
-      act(() => {
+        const button = screen.getByText(/Login/i);
         fireEvent.click(button);
       });
       expect(MockAxios.post).toHaveBeenCalled();
+    });
+    it('should navigate user to sso login page', () => {
+      const button = screen.getByText(/single sign on/i);
+
+      expect(button.href.includes('/sso')).toBeTruthy();
     });
   });
 });
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,24 +1,18 @@
 import { LoginIcon } from '@heroicons/react/outline';
 import { authLogin } from '@/config/endpoints';
+import { axiosInstance } from '@/config/axiosConfig';
 import { useAuth } from '@/contexts/AuthContext';
 import { useRouter } from 'next/router';
-import ActionButton from '@/components/buttons/ActionButton';
 import DefaultLayout from '@/components/layouts/DefaultLayout';
 import Image from 'next/image';
-import InputField from '@/components/inputs/InputField';
 import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
 import logo from '@/public/logo.png';
-import useField from '@/hooks/useField';
-
-// utils
-import { axiosInstance } from '@/config/axiosConfig';
-import { isValidEmail } from '@/utils/validation';
 
 export default function Login() {
   const router = useRouter();
-  const { user, login, logout } = useAuth();
-  const { fields: credentials, updateKeyValuePair } = useField({
+  const { user, login } = useAuth();
+  const [credentials, setCredentials] = useState({
     username: '',
     password: '',
   });
@@ -27,34 +21,29 @@ export default function Login() {
     if (user) router.push('/');
   }, []);
 
-  const setCredentials = (event) => {
-    updateKeyValuePair(event.target.name, event.target.value);
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setCredentials((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
   };
   const [errorMsg, setErrorMsg] = useState();
 
-  const handleLogin = () => {
+  const handleLogin = (event) => {
+    event.preventDefault();
     if (credentials.username === '' || credentials.password === '') {
       setErrorMsg('All fields required');
-    } else if (!isValidEmail(credentials.username)) {
-      setErrorMsg('Please enter a valid email address');
-    } else {
-      axiosInstance
-        .post(authLogin, credentials)
-        .then((res) => {
-          login(res.data);
-          router.push('/');
-        })
-        .catch((error) => {
-          // logout();
-          setErrorMsg('Invalid credentials');
-        });
     }
-  };
-
-  const handleEnterKey = (event) => {
-    if (event.key === 'Enter') {
-      handleLogin();
-    }
+    axiosInstance
+      .post(authLogin, credentials)
+      .then((res) => {
+        login(res.data);
+        router.push('/');
+      })
+      .catch((error) => {
+        setErrorMsg('Invalid credentials');
+      });
   };
 
   return (
@@ -77,43 +66,35 @@ export default function Login() {
             </Link>
           </span>
         </div>
-        <div className='w-1/3 p-8 mx-auto mt-10 bg-white flex flex-col items-center justify-between shadow-md rounded-md'>
-          <div
-            role='button'
-            className='space-y-4 mb-4'
-            onKeyPress={handleEnterKey}
-            tabIndex='0'
-          >
-            <input
-              type='text'
-              title='username'
-              name='username'
-              id='username'
-              placeholder='Email'
-              value={credentials.username}
-              onChange={setCredentials}
-              className='shadow focus:shadow-md rounded-md p-2 w-full border border-gray-200 text-gray-700 focus:ring-2 ring-blue-400 outline-none  transition-all  duration-200'
-            />
-
-            <InputField
-              type={'password'}
-              value={credentials.password}
-              placeholder='Password'
-              name='password'
-              onChange={(event) => setCredentials(event)}
-            />
-          </div>
-          <div className='mb-4'>
-            {errorMsg && <p className={'text-red-600'}>{errorMsg}</p>}
-          </div>
+        <form
+          onSubmit={handleLogin}
+          onChange={(event) => handleChange(event)}
+          className='w-1/3 mx-auto bg-white p-8 rounded shadow grid gap-4 mt-10'
+        >
+          <input
+            type='text'
+            name='username'
+            title='username'
+            placeholder='Email'
+            className='shadow focus:shadow-md rounded-md p-2 w-full border border-gray-200 text-gray-700 focus:ring-2 ring-blue-400 outline-none  transition-all  duration-200'
+          />
+          <input
+            type='password'
+            name='password'
+            title='password'
+            placeholder='Password'
+            className='shadow focus:shadow-md rounded-md p-2 w-full border border-gray-200 text-gray-700 focus:ring-2 ring-blue-400 outline-none  transition-all  duration-200'
+          />
+          <span>{errorMsg}</span>
           <button
-            className='max-w-max items-center inline-flex gap-2 text-blue-400 rounded-md hover:shadow-md bg-blue-50 hover:bg-blue-400 hover:text-white px-4 py-2 transform transition-all duration-150 ease-in-out border-blue-400 border-2 outline-none focus:ring-2 ring-blue-400'
-            onClick={() => handleLogin()}
-            id={'login-button'}
+            className='mt-4 mx-auto max-w-max items-center inline-flex gap-2 text-blue-400 rounded-md hover:shadow-md bg-blue-50 hover:bg-blue-400 hover:text-white px-4 py-2 transform transition-all duration-75 ease-in-out border-blue-400 border-2 outline-none focus:ring-2 ring-blue-400'
+            type='submit'
+            id='login-button'
           >
             <LoginIcon className='w-5 h-5' />
             Login
           </button>
+
           <p className={'my-8 relative border-b-2 w-full'}>
             <span className='absolute top-1/2 left-1/2 transform text-center -translate-x-1/2 -translate-y-1/2 bg-white px-2 w-max'>
               or continue with
@@ -122,13 +103,11 @@ export default function Login() {
           <a
             href='/sso'
             id={'sso-button'}
-            className={
-              'bg-blue-500 py-2 px-4 mt-5 rounded inline-block text-white hover:opacity-90 hover:shadow transform transition-all duration-100 ease-in-out font-semibold'
-            }
+            className='bg-blue-500 py-2 px-4 rounded inline-block text-white hover:opacity-90 hover:shadow transform transition-all duration-100 ease-in-out font-semibold max-w-max mx-auto'
           >
             Single Sign On
           </a>
-        </div>
+        </form>
       </div>
     </DefaultLayout>
   );
