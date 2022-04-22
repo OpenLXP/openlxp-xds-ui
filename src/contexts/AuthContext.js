933e299b022f9499ffe0f9ea98bdda263e1c2f77
rglasgow-deloitte <rglasgow@deloitte.com>
2022-04-22 15:10:16 -0400

    Updates register.js according to foritify.

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -34,7 +34,7 @@ export function AuthProvider({ children }) {
     axiosInstance
       .post(`${backendHost}/api/auth/logout`)
       .then((res) => removeLocal())
-      .catch((error) => console.log(error));
+      .catch((error) => );
     removeLocal();
   };
 
