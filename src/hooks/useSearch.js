d9b9876738b209c20b47ec1cf86fad4802d95a13
rglasgow-deloitte <rglasgow@deloitte.com>
2022-04-25 16:04:18 -0400

    Adds optimistic updating to search and lists.

--- src/hooks/useSearch.js
+++ src/hooks/useSearch.js
@@ -2,13 +2,20 @@ import { tenMinutes } from '@/config/timeConstants';
 import { useQuery, useQueryClient } from 'react-query';
 import axios from 'axios';
 
+const getSearchResults = (searchTerm) => {
+  return axios.get(searchTerm).then((res) => res.data);
+};
+
 export function useSearch(url) {
   const queryClient = useQueryClient();
-  return useQuery(
-    ['search', url],
-    () => axios.get(url).then((res) => res.data),
-    {
-      staleTime: tenMinutes,
-    }
-  );
+  return useQuery(['search', url], () => getSearchResults(url), {
+    staleTime: tenMinutes,
+    onSuccess: (data) => {
+      // add each of the hits to the query client as a course
+      // the hit.meta.id is the same as the metadata_key_hash
+      data?.hits.forEach((hit) => {
+        queryClient.setQueryData(['course', hit.meta.id], hit);
+      });
+    },
+  });
 }
