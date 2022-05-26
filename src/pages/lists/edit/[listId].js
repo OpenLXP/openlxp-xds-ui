f3e2961d1a95c1a0c0bf54460d99afac94e23cc1
JDTobin <90791836+JDTobin@users.noreply.github.com>
2022-05-26 14:03:21 -0400

    Remove list visibility toggle from list edit page (#85)
    Co-authored-by: schittlur <78377530+schittlur@users.noreply.github.com>

--- src/pages/lists/edit/[listId].js
+++ src/pages/lists/edit/[listId].js
@@ -24,36 +24,6 @@ export function getServerSideProps({ query }) {
   };
 }
 
-export function ToggleButton() {
-  return (
-    <div className='flex gap-2 items-center font-semibold text-lg'>
-      <label htmlFor='public toggle'>Set Visibility:</label>
-      <Switch
-        title='toggle'
-        checked={currentListInfo.public}
-        onChange={toggleListVisibility}
-        className={`${currentListInfo.public ? 'bg-green-500' : 'bg-gray-400'}
-    relative inline-flex flex-shrink-0 h-[28px] w-[48px] border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus-visible:ring-4 focus-visible:ring-offset-1 focus-visible:ring-blue-400 focus-visible:ring-opacity-75`}
-      >
-        <span className='sr-only'>Use setting</span>
-        <span
-          aria-hidden='true'
-          className={`${
-            currentListInfo.public ? 'translate-x-[20px]' : 'translate-x-0'
-          }
-      pointer-events-none inline-flex h-[24px] w-[24px] rounded-full bg-white shadow-lg transform ring-0 transition ease-in-out duration-200 justify-center items-center`}
-        >
-          {currentListInfo.public ? (
-            <EyeIcon className='h-4 text-gray-700' />
-          ) : (
-            <EyeOffIcon className='h-4 text-gray-500' />
-          )}
-        </span>
-      </Switch>
-    </div>
-  );
-}
-
 export default function EditList({ listId }) {
   const router = useRouter();
   const { user } = useAuth();
@@ -164,7 +134,7 @@ export default function EditList({ listId }) {
 
       <form onSubmit={submitData} onReset={resetData} className='mt-10'>
         {/* toggle switch with description*/}
-        <PublicPrivateToggle currentListInfo={currentListInfo} toggleListVisibility={toggleListVisibility}/>
+        {/* <PublicPrivateToggle currentListInfo={currentListInfo} toggleListVisibility={toggleListVisibility}/> */}
 
         {/* Title & description input */}
         <div className='grid grid-cols-2 gap-6 mt-10'>
