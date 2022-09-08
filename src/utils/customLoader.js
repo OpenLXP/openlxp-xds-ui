919cd3495996a234664970362bdaac312fc12445
James Tobin <jametobin@deloitte.com>
2022-09-08 19:26:38 +0000

    Update customLoader.js

--- src/utils/customLoader.js
+++ src/utils/customLoader.js
@@ -4,5 +4,5 @@ export function myDefaultLoader({ config , src , width , quality  }) {
         // through the built-in Image Optimization API.
         return src;
     }
-    return `${config.path}?url=${encodeURIComponent(src.replace("/ecc-openlxp-xds-ui/", "/"))}&w=${width}&q=${quality || 75}`;
+    return `${config.path}?url=${encodeURIComponent(src.replace("ecc-openlxp-xds-ui", ""))}&w=${width}&q=${quality || 75}`;
 }
