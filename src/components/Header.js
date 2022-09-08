50ad8127d66171861b4c07f8ad3018d1375dc675
James Tobin <jametobin@deloitte.com>
2022-09-08 17:56:29 +0000

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router';
 import Image from 'next/image';
 import Link from 'next/link';
 import UserMenu from '@/components/menus/UserMenu';
+import logo from '@/public/logo.png';
 
 const menuItems = [
   {
@@ -62,7 +63,7 @@ export default function Header() {
                 id={'homepage-button'}
                 className={'cursor-pointer'}
               >
-                <Image src={'/ecc-openlxp-xds-ui/public/logo.png'} alt={'home'} height={'60'} width={'60'} />
+                <Image src={logo} alt={'home'} height={'60'} width={'60'} />
               </button>
             </Link>
             {menuItems.map((item) => {
