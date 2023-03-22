dab8fcc703d923f9682566c728073673a0d1ca74
JDTobin <90791836+JDTobin@users.noreply.github.com>
2023-03-22 11:11:13 -0400

    update keys for components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,7 +18,7 @@ export default function Footer({ location }) {
     links.map((link, index) => {
       return (
         <a
-          key={index}
+          key={link.name + ' key'}
           className='text-center text-gray-500 text-base p-1 hover:text-gray-900 h-auto hover:text-shadow-md transform transition-all duration-75 ease-in-out'
           href={link.url}
         >
