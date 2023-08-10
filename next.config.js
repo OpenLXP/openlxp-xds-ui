3283900542a1b352b1bc62d64d10541fbb0dea64
Luis Reyes Madrid <lreyesmadrid@deloitte.com>
2023-08-10 15:25:21 +0000

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,28 @@
 module.exports = {
 
     basePath: '/ecc-openlxp-xds-ui',
+    reactStrictMode: true,
+    swcMinify: true,
+    // Adding policies:
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'X-Frame-Options',
+                        value: 'DENY',
+                    },
+                    {
+                        key: 'X-Content-Type-Options',
+                        value: 'nosniff',
+                    },
+                    {
+                        key: 'Referrer-Policy',
+                        value: 'origin-when-cross-origin',
+                    },
+                ],
+            },
+        ];
+    },
 }
