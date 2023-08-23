08198c394bee0e2336810c9c3ab1369328aaeb36
LFRMadrid <73301821+LFRMadrid@users.noreply.github.com>
2023-08-23 15:17:37 -0400

    testing verified signature one more time

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
     reactStrictMode: true,
     swcMinify: true,
     // Adding policies:
-    // Adding comment here to test verified signature
+    // Adding comment here to test verified signature... one more time
     async headers() {
         return [
             {
