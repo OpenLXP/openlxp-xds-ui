3a20b956181177f6dcce1609102ec057b54f8147
LFRMadrid <lreyesmadrid@deloitte.com>
2023-08-24 14:23:27 -0400

    testing verified email

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
     reactStrictMode: true,
     swcMinify: true,
     // Adding policies:
-    // Adding comment here to test verified signature... 
+    // Configured email to test verified signature
     async headers() {
         return [
             {
