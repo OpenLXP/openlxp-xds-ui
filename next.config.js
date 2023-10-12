f44a1fd2af726dc6de4c08d99286ea86118e483f
Simeranjeet Sandhu <simeransandhu@deloitte.com>
2023-10-12 10:41:53 -0700

    Reverting CSP Headers to debug UI

--- next.config.js
+++ next.config.js
@@ -19,10 +19,6 @@ const nextConfig = {
                         key: 'Referrer-Policy',
                         value: 'origin-when-cross-origin',
                     },
-                    {
-                        key: 'Content-Security-Policy',
-                        value: "script-src 'self' https://ecc.staging.dso.mil; img-src 'self' data: https:; script-src-elem 'self'; font-src 'self' https://fonts.gstatic.com"
-                    },
                 ],
             },
         ];
