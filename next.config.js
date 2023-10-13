3e07c36de18531c89e2ffaf65ea3408e1be09b43
Simeranjeet Sandhu <simeransandhu@deloitte.com>
2023-10-13 13:12:15 -0700

    updated next.config.js

--- next.config.js
+++ next.config.js
@@ -21,7 +21,7 @@ const nextConfig = {
                     },
                     {
                         key: 'Strict-Transport-Security',
-                        value: 'max-age=63072000; includeSubDomains; preload'
+                        value: 'max-age=63072000; includeSubDomains; preload',
                     },
                 ],
             },
