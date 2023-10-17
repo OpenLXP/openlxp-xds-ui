b77efcd78637c6fccbc1c9ab30661745a0929d11
Simeranjeet Sandhu <simeransandhu@deloitte.com>
2023-10-17 14:04:44 -0700

    added import to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,20 @@
 'use strict';
 
-import { useAuth } from '@/contexts/AuthContext';
-import { useRouter } from 'next/router';
-import { xAPISendStatement } from '@/utils/xapi/xAPISendStatement';
+import React, { useCallback } from 'react';
+
 import CourseSpotlight from '@/components/cards/CourseSpotlight';
 import Footer from '@/components/Footer';
 import Head from 'next/head'
 import Header from '@/components/Header';
 import Image from 'next/image';
-import React, { useCallback } from 'react';
 import SearchBar from '@/components/inputs/SearchBar';
 import logo from '@/public/logo.png';
+import myDefaultLoader from '@/utils/customLoader';
+import { useAuth } from '@/contexts/AuthContext';
 import useField from '@/hooks/useField';
+import { useRouter } from 'next/router';
 import useSpotlightCourses from '@/hooks/useSpotlightCourses';
+import { xAPISendStatement } from '@/utils/xapi/xAPISendStatement';
 
 export default function Home() {
   const router = useRouter();
