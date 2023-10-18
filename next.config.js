83b2ba225f848f616602caa7c97863f8f8a52a6b
Simeranjeet Sandhu <simeransandhu@deloitte.com>
2023-10-18 16:05:32 -0700

    Debugging image rendering issue - modified Dockerfile with sed

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@ const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
     basePath: '/ecc-openlxp-xds-ui',
+    images: {
+        // Specify a custom loader to adjust the image URL
+        loader: 'default', // or 'imgix' or 'cloudinary', depending on your configuration
+        path: 'https://ecc.staging.dso.mil/ecc-openlxp-xds-ui/_next/static/media', // Your new base path
+      },
 
     // Adding policies:
     async headers() {
