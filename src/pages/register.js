4ace0585520dff4a546a3e31a681e1f1bb81386f
Simeranjeet Sandhu <simeransandhu@deloitte.com>
2023-10-19 10:34:13 -0700

    debugging image rendering issue - Image src updated in .js files

--- src/pages/register.js
+++ src/pages/register.js
@@ -238,7 +238,7 @@ export default function Register() {
   return (
     <DefaultLayout>
       <div className='text-center mt-10'>
-        <Image src={logo} alt='logo' width={100} height={100} priority={true}/>
+        <Image src={'/ecc-openlxp-xds-ui/public/logo.png'} alt='logo' width={100} height={100} priority={true}/>
         <h1 className='font-bold text-xl'>Create your account</h1>
         <p className='text-sm'>
           or&nbsp;
