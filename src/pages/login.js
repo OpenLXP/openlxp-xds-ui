82811b99dd6f30dc184d3ed5f3d92bbe2ed3cabf
Simeranjeet Sandhu <simeransandhu@deloitte.com>
2023-10-18 20:37:06 -0700

    Testing basePath for logo

--- src/pages/login.js
+++ src/pages/login.js
@@ -2,17 +2,17 @@
 
 import React, { useEffect, useState } from 'react';
 
-import DefaultLayout from '@/components/layouts/DefaultLayout';
-import Image from 'next/image';
-import Link from 'next/link';
 import { LoginIcon } from '@heroicons/react/outline';
 import { authLogin } from '@/config/endpoints';
 import { axiosInstance } from '@/config/axiosConfig';
-import logo from '@ecc-openlxp-xds-ui/public/logo.png';
-import myDefaultLoader from '@/utils/customLoader';
 import { useAuth } from '@/contexts/AuthContext';
 import { useConfig } from '@/hooks/useConfig';
 import { useRouter } from 'next/router';
+import DefaultLayout from '@/components/layouts/DefaultLayout';
+import Image from 'next/image';
+import Link from 'next/link';
+import logo from '@ecc-openlxp-xds-ui/public/logo.png';
+import myDefaultLoader from '@/utils/customLoader';
 
 export default function Login() {
   const router = useRouter();
