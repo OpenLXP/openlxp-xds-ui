80e060ff62e5e5bf0b54b9171ed8957f41abb7e4
Simeranjeet Sandhu <simeransandhu@deloitte.com>
2023-11-03 11:42:47 -0700

    Testing CSP Headers

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,8 +6,8 @@ import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
 import React, { useState } from 'react';
 
 import { AuthProvider } from '../contexts/AuthContext';
-import { ReactQueryDevtools } from 'react-query/devtools';
 import Head from 'next/head'
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 // contexts
 
@@ -34,7 +34,7 @@ export default function MyApp({ Component, pageProps }) {
       <QueryClientProvider client={queryClient}>
         <Hydrate state={pageProps['dehydratedState']}>
           <Head>
-            <meta httpEquiv="Content-Security-Policy" content="script-src 'self' https://unpkg.com https://ecc.staging.dso.mil https://ecc.staging.dso.mil/; img-src 'self' data: https: https://unpkg.com https://ecc.staging.dso.mil; "/>
+            <meta httpEquiv="Content-Security-Policy" content="script-src 'none'; img-src 'none'; "/>
             <title>Experience Discovery Service</title>
             <link rel="icon" href="/favicon.ico" />
           </Head>
