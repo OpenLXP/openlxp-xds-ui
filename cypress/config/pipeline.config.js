a9f45253c34f5a524aa58b8699f84bd4ec5090cd
JDTobin <90791836+JDTobin@users.noreply.github.com>
2023-12-20 16:00:10 -0500

    change env loading

--- cypress/config/pipeline.config.js
+++ cypress/config/pipeline.config.js
@@ -2,6 +2,16 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: process.env.ZAP_URL,
+    setupNodeEvents(on, config) {
+        console.log(config) // see everything in here!
+  
+        // modify config values
+        config.defaultCommandTimeout = 10000
+        config.baseUrl = Cypress.env('ZAP_URL')
+  
+        // IMPORTANT return the updated config object
+        return config
+      },
+  
   },
 })
