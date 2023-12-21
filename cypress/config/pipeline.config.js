02e6f6d2f8933e4919a2e9c3e4e65411ddf75e43
JDTobin <90791836+JDTobin@users.noreply.github.com>
2023-12-21 10:09:48 -0500

    set baseUrl from ZAP_URL

--- cypress/config/pipeline.config.js
+++ cypress/config/pipeline.config.js
@@ -7,7 +7,7 @@ module.exports = defineConfig({
   
         // modify config values
         config.defaultCommandTimeout = 10000
-        // config.baseUrl = config.env.ZAP_URL
+        config.baseUrl = config.env.ZAP_URL
   
         // IMPORTANT return the updated config object
         return config
