de7ccd060c6009e8793aae91e95fc8a742cf8d31
JDTobin <90791836+JDTobin@users.noreply.github.com>
2024-01-03 11:05:47 -0500

    fix base url add tests

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -10,5 +10,50 @@ describe('homepage', () => {
     it('Take Screenshot of homepage', () => {
         // Check the first primary menu link.
         cy.screenshot('homepage')
+        expect(true).to.be.false
     })
+
+    // remote file include https://sdelements.il2.dso.mil/bunits/platform1/ecc/open-lxp-xds-ui/tasks/phase/testing/387-T122/
+    // authorization https://sdelements.il2.dso.mil/bunits/platform1/ecc/open-lxp-xds-ui/tasks/phase/testing/387-T85/
+    // XSS https://sdelements.il2.dso.mil/bunits/platform1/ecc/open-lxp-xds-ui/tasks/phase/testing/387-T89/
+    // check session https://sdelements.il2.dso.mil/bunits/platform1/ecc/open-lxp-xds-ui/tasks/phase/testing/387-T128/
+    // check storage https://sdelements.il2.dso.mil/bunits/platform1/ecc/open-lxp-xds-ui/tasks/phase/testing/387-T321/
+
+    it('Check local and session storage', () => {
+        // Non authenticated should be empty
+        cy.getAllLocalStorage().should('be.empty')
+        cy.getAllSessionStorage().should('be.empty')
+    })
+
+    it('Check local and session storage authenticated', () => {
+        // Authenticated should store user info in session
+        // TODO ADD LOGIN
+
+        cy.getAllLocalStorage().should('be.empty')
+        cy.getAllSessionStorage().should('contain.value', 'user')
+    })
+
+    // check rbac https://sdelements.il2.dso.mil/bunits/platform1/ecc/open-lxp-xds-ui/tasks/phase/testing/387-T2276/
+    // check authentication https://sdelements.il2.dso.mil/bunits/platform1/ecc/open-lxp-xds-ui/tasks/phase/testing/387-T2277/
+    // access authorized pages https://sdelements.il2.dso.mil/bunits/platform1/ecc/open-lxp-xds-ui/tasks/phase/testing/387-T84/
+    // null byte check https://sdelements.il2.dso.mil/bunits/platform1/ecc/open-lxp-xds-ui/tasks/phase/testing/387-T127/
+    // access local files https://sdelements.il2.dso.mil/bunits/platform1/ecc/open-lxp-xds-ui/tasks/phase/testing/387-T297/
+    // input validation https://sdelements.il2.dso.mil/bunits/platform1/ecc/open-lxp-xds-ui/tasks/phase/testing/387-T519/
+    // check meta tag https://sdelements.il2.dso.mil/bunits/platform1/ecc/open-lxp-xds-ui/tasks/phase/testing/387-T132/
+    // no-cache https://sdelements.il2.dso.mil/bunits/platform1/ecc/open-lxp-xds-ui/tasks/phase/testing/387-T112/
+
+    it('Check no-cache headers', () => {
+        // Check for cache control in header set to no-cache
+        // ticket is for requests not responses????  need to figure out how to check requests
+        cy.request('/').its('headers').should('have.keys', 'Cache-Control').and('deep.equal', {'Cache-Control': 'no-cache'})
+    })
+
+    // check CSP https://sdelements.il2.dso.mil/bunits/platform1/ecc/open-lxp-xds-ui/tasks/phase/testing/387-T332/
+    
+    it('Check CSP headers', () => {
+        // Check for CSP control in header set to no-cache
+        // ticket is for requests not responses????  need to figure out how to check requests
+        cy.request('/').its('headers').should('have.keys', 'Content-Security-Policy').and('deep.string', {'Content-Security-Policy': 'self'})
+    })
+
 })
