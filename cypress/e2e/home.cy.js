0df681194845984458ffc934b7d8d730d81b659f
Simeranjeet Sandhu <simeransandhu@deloitte.com>
2024-01-16 07:28:49 -0800

    Uncommented homepage test

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,13 +1,13 @@
 /// <reference types="Cypress" />
 
 describe('homepage', () => {
-  // beforeEach(() => {
-  //     // Cypress starts out with a blank slate for each test
-  //     // so we must tell it to visit our website with the `cy.visit()` command.
-  //     // Since we want to visit the same URL at the start of all our tests,
-  //     // we include it in our beforeEach function so that it runs before each test
-  //     cy.visit('/')
-  // })
+  beforeEach(() => {
+      // Cypress starts out with a blank slate for each test
+      // so we must tell it to visit our website with the `cy.visit()` command.
+      // Since we want to visit the same URL at the start of all our tests,
+      // we include it in our beforeEach function so that it runs before each test
+      cy.visit('/')
+  })
 
   // remote file include https://sdelements.il2.dso.mil/bunits/platform1/ecc/open-lxp-xds-ui/tasks/phase/testing/387-T122/
   it('Check ssi injection', () => {
@@ -22,6 +22,7 @@ describe('homepage', () => {
     ];
     const d = new Date();
     let day = weekday[d.getDay()];
+
     // encoded <!--#echo var="DATE_LOCAL" -->
     cy.request('/search?keyword=<%21--%23echo%20var%3D"DATE_LOCAL"%20-->&p=1');
     cy.contains(day).should('not.exist');
