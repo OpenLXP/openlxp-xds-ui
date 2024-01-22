c710186dad0352607d890fe1efe3e73080a0d8ba
Simeranjeet Sandhu <simeransandhu@deloitte.com>
2024-01-22 09:27:32 -0800

    e2e:troubleshooting ssi injection

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -26,7 +26,7 @@ describe('homepage', () => {
     // encoded <!--#echo var="DATE_LOCAL" -->
     cy.visit('/search?keyword=<%21--%23echo%20var%3D"DATE_LOCAL"%20-->&p=1');
     cy.contains(day).should('not.exist');
-    cy.get('search-bar').should('have.value', '<!--#echo var="DATE_LOCAL" -->');
+    cy.get('input').should('have.value', '<!--#echo var="DATE_LOCAL" -->');
   });
 
   // access authorized pages https://sdelements.il2.dso.mil/bunits/platform1/ecc/open-lxp-xds-ui/tasks/phase/testing/387-T84/
