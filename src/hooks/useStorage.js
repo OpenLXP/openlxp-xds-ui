19eecd482c83b799829eaa6082975216a5214d6a
Simeranjeet Sandhu <simeransandhu@deloitte.com>
2024-11-14 13:51:30 -0800

    Disabling lint rule

--- src/hooks/useStorage.js
+++ src/hooks/useStorage.js
@@ -36,6 +36,7 @@ function useStorage(key, defaultValue, storageObject) {
 
 export function useLocalStorage(key, defaultValue) {
   if (typeof window !== 'undefined') {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
     const useLocal = useStorage(key, defaultValue, localStorage)
     return useLocal;
   }
@@ -44,8 +45,10 @@ export function useLocalStorage(key, defaultValue) {
 
 export function useSessionStorage(key, defaultValue) {
   if (typeof window !== 'undefined') {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
     const useSession = useStorage(key, defaultValue, sessionStorage)
     return useSession;
   }
   return [defaultValue]
 }
+
