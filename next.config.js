4ec115c988adde4d8272dac6766e1dda67c8bb47
Simeranjeet Sandhu <simeransandhu@deloitte.com>
2025-01-02 13:44:11 -0800

    Package.json from P1

---
+++ next.config.js
@@ -0,0 +1,60 @@
+const { createSecureHeaders } = require("next-secure-headers");
+
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    // basePath: '/ecc-openlxp-xds-ui',
+
+    // Adding policies:
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: createSecureHeaders({
+                    contentSecurityPolicy: {
+                        directives: {
+                            defaultSrc: [
+                                "'self'",
+                                "https://ecc.staging.dso.mil",
+                                "https://ecc.staging.dso.mil/ecc-openlxp-xds/",
+                                "https://ecc.apps.dso.mil",
+                                "https://ecc.apps.dso.mil/ecc-openlxp-xds-ui/"
+                            ],
+                            styleSrc: [
+                                "'self'",
+                                "https://ecc.apps.dso.mil",
+                                "https://ecc.apps.dso.mil/ecc-openlxp-xds-ui/",
+                                "https://ecc.staging.dso.mil", 
+                                "https://fonts.googleapis.com"
+                            ],
+                            imgSrc: ["'self'",
+                                    "data:",
+                                    "data:*",
+                                    "https://www.jcs.mil",
+                                    "https://www.aetc.af.mil",
+                                    "https://prod-discovery.edx-cdn.org",
+                            ],
+                            fontSrc: [
+                                "'self'", 
+                                "https://fonts.gstatic.com"
+                            ],
+                            frameAncestors: [
+                                "'self'",
+                                "https://ecc.staging.dso.mil",
+                                "https://ecc.apps.dso.mil",
+                                "https://ecc.apps.dso.mil/ecc-openlxp-xms-ui/"
+                            ]
+                        },
+                        frameGuard: "deny",
+                        noopen: "noopen",
+                        nosniff: "nosniff",
+                        xssProtection: "sanitize",
+                        referrerPolicy: "origin-when-cross-origin",
+                    }
+                })
+            },
+        ];
+    },
+}
+
+module.exports = nextConfig
