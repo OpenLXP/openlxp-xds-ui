8840264d30303d0b63ee9077f7b61e0a28c03792
JDTobin <90791836+JDTobin@users.noreply.github.com>
2025-01-03 14:02:16 -0500

    migrate gh changes

---
+++ src/hooks/useAllNotificRead.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import { axiosInstance } from '@/config/axiosConfig';
+import { allRead } from '@/config/endpoints';
+
+export const getAllRead = () => {
+  return () => axiosInstance.get(allRead).then((res) => res.data);
+};
+
