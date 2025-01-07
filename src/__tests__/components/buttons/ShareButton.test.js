a8c76be697b73d6b184e69fedc75e35b3ba6e95f
schittlur <78377530+schittlur@users.noreply.github.com>
2025-01-07 13:46:03 -0500

    imports added

--- src/__tests__/components/buttons/ShareButton.test.js
+++ src/__tests__/components/buttons/ShareButton.test.js
@@ -2,7 +2,7 @@
 
 import '@testing-library/jest-dom'
 import { act, fireEvent, render } from '@testing-library/react';
-import { useUnauthenticatedUser, useAuthenticatedUser } from '@/__mocks__/predefinedMocks';
+import { useAuthenticatedUser, useUnauthenticatedUser } from '@/__mocks__/predefinedMocks';
 import ShareButton from '@/components/buttons/ShareBtn';
 import mock, { xAPISendStatement } from '@/utils/xapi/xAPISendStatement';
 
@@ -17,6 +17,7 @@ describe('ShareButton', () => {
     expect(screen.getByText('Share')).toBeInTheDocument();
 
     fireEvent.click(screen.getByTestId("shareButton"));
+
     // act(() => {
     //   fireEvent.click(screen.getByText(/Copy/i));
     // });
