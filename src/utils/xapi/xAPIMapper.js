454c41dffecf212c3dcb360729f167257ac87a6e
Milton Reder <milt@yetanalytics.com>
2025-01-17 14:34:07 -0500

    Use XDS xAPI forwarding

--- src/utils/xapi/xAPIMapper.js
+++ src/utils/xapi/xAPIMapper.js
@@ -1,27 +1,12 @@
-import { endpoint, key, secret } from '@/config/xAPIConfig';
-import XAPI from "@xapi/xapi";
-
-let xapiInstance = null;
+import { axiosInstance } from '../../config/axiosConfig.js';
+import { statementsUrl } from '../../config/endpoints';
 
 class XAPIMapper {
-
-  constructor() {
-    const auth = XAPI.toBasicAuth(key, secret);
-
-    if (!xapiInstance) { xapiInstance = this; }
-    if (endpoint) {
-      this.xapiInstance = new XAPI({
-        endpoint: endpoint,
-        auth: auth
-      });
-    }
-
-    return xapiInstance;
-  }
-
   sendStatement = ({ statement }) => {
-    return this.xapiInstance?.sendStatement({ statement }).catch((err) => console.error(err));
-  }
+    return axiosInstance
+      .post(statementsUrl, [statement])
+      .catch((err) => console.error(err));
+  };
 }
 
-export default (new XAPIMapper);
+export default new XAPIMapper();
