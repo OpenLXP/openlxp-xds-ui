ab8d6a1a0749aabcabba79c0f1d53e4f7b21ef1a
Milton Reder <milt@yetanalytics.com>
2025-01-30 16:56:50 -0500

    fix search result test

--- src/__tests__/components/cards/SearchResult.test.js
+++ src/__tests__/components/cards/SearchResult.test.js
@@ -2,6 +2,7 @@ import { QueryClientWrapper } from '@/__mocks__/queryClientMock';
 import { act, fireEvent, render } from '@testing-library/react';
 import {
   useAuthenticatedUser,
+  useMockConfig,
   useMockCreateUserList,
   useMockUpdateUserList,
   useMockUserOwnedListsWithoutData,
@@ -23,6 +24,7 @@ beforeEach(() => {
   useMockUserOwnedListsWithoutData();
   useMockUpdateUserList();
   useMockCreateUserList();
+  useMockConfig();
 });
 
 describe('SearchResult', () => {
